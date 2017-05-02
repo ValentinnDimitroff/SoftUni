9923bbf583ca1be80986b305a061e58e9d00b3a9
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-02 15:13:18 +0300

    C# Advanced - Truck Tour 20pts

---
+++ 03 - C# Advanced/01.Stacks And Queues/StacksAndQueues/04.BasicQueueOperations/04.BasicQueueOperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputParams = Console.ReadLine()
+                   .Split(new[] { ' ' },
+                   StringSplitOptions.RemoveEmptyEntries)
+                   .Select(int.Parse)
+                   .ToArray();
+
+            var count = inputParams[0];
+            var dequeueTimes = inputParams[1];
+            var valueToCheck = inputParams[2];
+
+            var inputNumbers = Console.ReadLine()
+                .Split(new[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var queue = new Queue<int>();
+
+            foreach (var number in inputNumbers)
+            {
+                queue.Enqueue(number);
+            }
+            for (int i = 0; i < dequeueTimes; i++)
+            {
+                queue.Dequeue();
+            }
+            var result = queue.ToArray();
+
+            if (result.Contains(valueToCheck))
+                Console.WriteLine("true");
+            else
+            {
+                if (result.Length > 0)
+                    Console.WriteLine(result.Min());
+                else Console.WriteLine("0");
+            }
+        }
+    }
+}
---
+++ 03 - C# Advanced/01.Stacks And Queues/StacksAndQueues/05.SequenceWithQueue/05.SequenceWithQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.SequenceWithQueue
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var startNumber = long.Parse(Console.ReadLine());
+            var dequeCount = 50;
+            var queue = new Queue<long>();
+            queue.Enqueue(startNumber);
+
+            for (int i = 0; i < dequeCount; i++)
+            {
+                var followingNumber = queue.Dequeue();
+                Console.Write( followingNumber + " ");
+
+                queue.Enqueue(followingNumber + 1);
+                queue.Enqueue(2 * followingNumber + 1);
+                queue.Enqueue(followingNumber + 2);
+            }
+        }
+    }
+}
