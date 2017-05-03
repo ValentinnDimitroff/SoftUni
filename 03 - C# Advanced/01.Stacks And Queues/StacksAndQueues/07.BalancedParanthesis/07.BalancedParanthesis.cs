bc53c2dc913edbf3fec1361e2b6e29847a2e9488
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-03 15:00:08 +0300

    C# Advanced - Balanced Paranthesis

---
+++ 03 - C# Advanced/01.Stacks And Queues/StacksAndQueues/07.BalancedParanthesis/07.BalancedParanthesis.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.BalancedParanthesis
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var paranthesisLine = Console.ReadLine();
+            var mismatch = false;
+            var openingCases = new[] { '{', '[', '(' };
+            var openedParanths = new Stack<char>();
+
+            foreach (var symbol in paranthesisLine)
+            {
+                if (openingCases.Contains(symbol))
+                {
+                    openedParanths.Push(symbol);
+                }
+                else
+                {
+                    if (openedParanths.Count != 0)
+                    {
+                        switch (symbol)
+                        {
+                            case ')':
+                                if (openedParanths.Pop() != '(')
+                                    mismatch = true;
+                                break;
+                            case ']':
+                                if (openedParanths.Pop() != '[')
+                                    mismatch = true;
+                                break;
+                            case '}':
+                                if (openedParanths.Pop() != '{')
+                                    mismatch = true;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        mismatch = true;
+                    }
+                }
+            }
+
+            if (mismatch)
+                Console.WriteLine("NO");
+            else
+                Console.WriteLine("YES");
+        }
+    }
+}
