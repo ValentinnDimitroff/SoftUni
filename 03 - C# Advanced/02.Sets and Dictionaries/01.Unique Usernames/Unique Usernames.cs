dbdca016602f9dd6e335ec5e71755a4f6a059c9c
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-11 19:41:58 +0300

    C# Advanced - Set and Dictionaries - Uniqie Usernames

---
+++ 03 - C# Advanced/02.Sets and Dictionaries/01.Unique Usernames/Unique Usernames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Unique_Usernames
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numberUsernames = int.Parse(Console.ReadLine());
+            var usernames = new HashSet<string>();
+
+            for (int i = 0; i < numberUsernames; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join("\n", usernames));
+        }
+    }
+}
