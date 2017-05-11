c17e2c74d551d64cfec309c96ba0e15d5a368c82
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-11 19:44:45 +0300

    C# Advanced - Set and Dictionaries - Fix Emails

---
+++ 03 - C# Advanced/02.Sets and Dictionaries/07.Fix Emails/Fix emails.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Fix_Emails
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var emailBook = new Dictionary<string, string>();
+            var message = Console.ReadLine();
+            var lineCounter = 1;
+            var person = "";
+
+            while (message != "stop")
+            {
+                if (lineCounter % 2 != 0)
+                {
+                    person = message;
+                }
+                else
+                {
+                    if (!message.EndsWith("uk") && !message.EndsWith("us"))
+                    {
+                        emailBook[person] = message;
+                    }
+                }
+                message = Console.ReadLine();
+                lineCounter++;
+            }
+
+            foreach (var pair in emailBook)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
