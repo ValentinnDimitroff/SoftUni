05e15ea84958b6d652f8aa7dd3a7979d1c1ff42f
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-11 19:43:38 +0300

    C# Advanced - Set and Dictionaries - A Miner Task

---
+++ 03 - C# Advanced/02.Sets and Dictionaries/06.A Miner Task/A Miner Task.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.A_Miner_Task
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var message = Console.ReadLine();
+            var lineCounter = 1;
+            var resources = new Dictionary<string, int>();
+            var currentResource = "";
+
+            while (message != "stop")
+            {
+                if (lineCounter % 2 != 0)
+                {
+                    currentResource = message;
+                }
+                else
+                {
+                    if (resources.ContainsKey(currentResource))
+                    {
+                        resources[currentResource] += int.Parse(message);
+                    }
+                    else
+                    {
+                        resources[currentResource] = int.Parse(message);
+                    }
+                }
+                message = Console.ReadLine();
+                lineCounter++;
+            }
+
+            foreach (var pair in resources)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
