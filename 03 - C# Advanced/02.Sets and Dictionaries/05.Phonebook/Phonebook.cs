855e9d31ed3502e5726df3c1b2fdcb26546a5674
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-11 19:43:17 +0300

    C# Advanced - Set and Dictionaries - Phonebook

---
+++ 03 - C# Advanced/02.Sets and Dictionaries/05.Phonebook/Phonebook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Phonebook
+{
+    public class Program
+    {
+        public static void Main()
+        {
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+            var message = "";
+            var phoneBook = new Dictionary<string, string>();
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
+            while (message != "search")
+            {
+                message = Console.ReadLine();
+                var contactParams = message
+                    .Split('-')
+                    .ToArray();
+                if (contactParams.Length == 2)
+                {
+                    phoneBook[contactParams[0]] = contactParams[1];
+                }
+            }
+            message = Console.ReadLine();
+            while (message != "stop")
+            {
+                if (phoneBook.ContainsKey(message))
+                {
+                    Console.WriteLine($"{message} -> {phoneBook[message]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Contact {message} does not exist.");
+                }
+                message = Console.ReadLine();
+            } 
+
+        }
+    }
+}
