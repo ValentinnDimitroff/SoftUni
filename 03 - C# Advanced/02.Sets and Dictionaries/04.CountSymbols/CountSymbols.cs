e5475aa154251b8f4fc9879c643ec8bee1777f07
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-11 19:42:51 +0300

    C# Advanced - Set and Dictionaries - Count Symbols

---
+++ 03 - C# Advanced/02.Sets and Dictionaries/04.CountSymbols/CountSymbols.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.CountSymbols
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputText = Console.ReadLine();
+            var symbols = new SortedDictionary<char, int>();
+            foreach (var symbol in inputText)
+            {
+                if (symbols.ContainsKey(symbol))
+                {
+                    symbols[symbol] += 1;
+                }
+                else
+                {
+                    symbols[symbol] = 1;
+                }
+            }
+
+            foreach (var element in symbols)
+            {
+                Console.WriteLine($"{element.Key}: {element.Value} time/s");
+            }
+        }
+    }
+}
