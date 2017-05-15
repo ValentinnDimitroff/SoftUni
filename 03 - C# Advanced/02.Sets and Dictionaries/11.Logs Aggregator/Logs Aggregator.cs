16eb056d9fd900d90e3543453355fd7a22c4b504
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-15 13:38:07 +0300

    C# Advanced - Sets and Dictionaries - Logs Aggregator

---
+++ 03 - C# Advanced/02.Sets and Dictionaries/11.Logs Aggregator/Logs Aggregator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Logs_Aggregator
+{
+    public class Logs_Aggregator
+    {
+        public static void Main()
+        {
+            var linesNumber = int.Parse(Console.ReadLine());
+            var logIps = new Dictionary<string, HashSet<string>>();
+            var loggedDuration = new Dictionary<string, int>();
+            for (int i = 0; i < linesNumber; i++)
+            {
+                var logParams = Console.ReadLine()
+                    .Split();
+                var ip = logParams[0];
+                var user = logParams[1];
+                var duration = int.Parse(logParams[2]);
+
+                if (logIps.ContainsKey(user))
+                {
+                    logIps[user].Add(ip);
+                    loggedDuration[user] += duration;
+                }
+                else
+                {
+                    logIps[user] = new HashSet<string>{ ip };
+                    loggedDuration[user] = duration;
+                }
+            }
+
+            foreach (var user in logIps.OrderBy(user => user.Key))
+            {
+                Console.WriteLine($"{user.Key}: {loggedDuration[user.Key]} [{string.Join(", ", user.Value.OrderBy(ip => ip))}]");
+            }
+        }
+    }
+}
