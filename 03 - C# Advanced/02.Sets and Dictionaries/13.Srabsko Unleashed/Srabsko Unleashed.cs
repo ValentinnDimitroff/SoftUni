be72859929cf5350056017cc5d33a77a12194e79
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-16 20:21:11 +0300

    C# Advanced - Sets and Dictionaries - Srabsko Unleashed

---
+++ 03 - C# Advanced/02.Sets and Dictionaries/13.Srabsko Unleashed/Srabsko Unleashed.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Srabsko_Unleashed
+{
+    public class SrabskoUnleashed
+    {
+        public static void Main()
+        {
+            var line = Console.ReadLine();
+            var statistics = new Dictionary<string, Dictionary<string, int>>();
+
+            while (line != "End")
+            {
+                string singer = "", venue = "";
+                int ticketCount = 0, ticketPrice = 0;
+
+                int atIndex = line.IndexOf('@');
+                if (atIndex > -1 && line[atIndex - 1] == ' ')
+                {
+                    singer = line.Substring(0, atIndex).TrimEnd();
+                }
+                else
+                {
+                    line = Console.ReadLine();
+                    continue;
+                }
+                atIndex++;
+                var venueTickets = line
+                    .Substring(atIndex, line.Length - atIndex)
+                    .Split();
+
+                try
+                {
+                    ticketPrice = int.Parse(venueTickets[venueTickets.Length - 1]);
+                    ticketCount = int.Parse(venueTickets[venueTickets.Length - 2]);
+                }
+                catch (Exception e)
+                {
+                    line = Console.ReadLine();
+                    continue;
+                }
+
+                var tempVenue = new StringBuilder();
+                for (int i = 0; i < venueTickets.Length - 2; i++)
+                {
+                    tempVenue.Append(venueTickets[i]);
+                    tempVenue.Append(" ");
+                }
+                venue = tempVenue.ToString().TrimEnd();
+
+                if (venue != "")
+                {
+                    if (statistics.ContainsKey(venue))
+                    {
+                        if (statistics[venue].ContainsKey(singer))
+                        {
+                            statistics[venue][singer] += ticketCount * ticketPrice;
+                        }
+                        else statistics[venue].Add(singer, ticketCount * ticketPrice);
+                    }
+                    else
+                    {
+                        statistics[venue] = new Dictionary<string, int> { { singer, ticketCount * ticketPrice } };
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+            PrintVenues(statistics);
+        }
+
+        private static void PrintVenues(Dictionary<string, Dictionary<string, int>> statistics)
+        {
+            foreach (var venue in statistics)
+            {
+                Console.WriteLine(venue.Key);
+                foreach (var singer in venue.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {singer.Key} -> {singer.Value}");
+                }
+            }
+        }
+    }
+}
