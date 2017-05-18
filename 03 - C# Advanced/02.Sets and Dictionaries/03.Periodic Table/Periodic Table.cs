380510df91931826cfb4658393bb98ff0bfecb82
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-18 20:16:05 +0300

    C# Advanced - Sets and Dictionaries - Periodic Table

---
+++ 03 - C# Advanced/02.Sets and Dictionaries/03.Periodic Table/Periodic Table.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Periodic_Table
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var elements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var compound = Console.ReadLine()
+                    .Split();
+
+                foreach (var element in compound)
+                {
+                    elements.Add(element);
+                }
+                //elements.UnionWith(compound); //Slower
+            }
+            
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
