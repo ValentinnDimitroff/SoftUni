ab6e6541e897e8cb915b642485fffb087944ff4c
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-05-25 16:02:11 +0300

    C# Advanced - Matrices - Diagonal Differnece

---
+++ 03 - C# Advanced/03.Matrices/002. Diagonal Difference/Diagonal Difference.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002.Diagonal_Difference
+{
+    public class DiagonalDifference
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var matrix = FillMatrix(n);
+            int sumPrimary = 0, sumSecondary = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        sumPrimary += matrix[row, col];
+                    }
+                    if (row == n - col - 1)
+                    {
+                        sumSecondary += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(sumPrimary - sumSecondary));
+        }
+
+        private static int[,] FillMatrix(int n)
+        {
+            var matrix = new int[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                var line = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
