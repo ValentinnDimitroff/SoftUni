a0576b0125ae3104306790c4429e81f9d0c49503
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-01 09:24:20 +0300

    C# Advanced - File Streams - Odd Lines

---
+++ 03 - C# Advanced/04.File Streams/01.Odd Lines/Odd Lines.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Odd_Lines
+{
+    public class OddLines
+    {
+        public static void Main()
+        {
+            using (var reader = new StreamReader("somefile.txt"))
+            {
+                int lineNumber = 1;
+                var line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (lineNumber % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    line = reader.ReadLine();
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
