0a33c8551240d829c11f7404c615666163dcc310
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-01 14:13:00 +0300

    C# Advanced - File Streams - Copy Binary File

---
+++ 03 - C# Advanced/04.File Streams/04.Copy Binary File/Copy Binary File.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Copy_Binary_File
+{
+    public class CopyBinaryFile
+    {
+        public static void Main()
+        {
+            using (var source = new FileStream("picture.jpg", FileMode.Open))
+            {
+                using (var destination = new FileStream("pic.jpg", FileMode.Create))
+                {
+                    var buffer = new byte[1024];
+                    var readBytes = 1;
+                    while (readBytes > 0)
+                    {
+                        readBytes = source.Read(buffer, 0, buffer.Length);
+                        destination.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+            
+            //using (var source = new BinaryReader("picture.jpg"))
+            //{
+                
+            //}
+        }
+    }
+}
