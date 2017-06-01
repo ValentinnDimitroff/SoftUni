ab472f86755f36d766cdc54a2ce7c6cceca272eb
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-01 14:12:08 +0300

    C# Advanced - File Streams - Line Numbers

---
+++ 03 - C# Advanced/04.File Streams/02.Line Numbers/Line Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _02.Line_Numbers
+{
+    public class LineNumbers
+    {
+        public static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("somefile.txt"))
+            {
+                using (var writer = new StreamWriter("numberedFile.txt"))
+                {
+                    int lineNumber = 1;
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        writer.WriteLine(lineNumber + line);
+                        line = reader.ReadLine();
+                        lineNumber++;
+                    }
+                }
+            }
+        }
+    }
+}
