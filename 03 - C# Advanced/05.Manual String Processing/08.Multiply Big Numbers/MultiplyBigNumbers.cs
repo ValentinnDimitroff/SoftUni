df93263f42353550ac0af4bd63feef97df5c901f
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:03:14 +0300

    C# Advanced - Manual String Processing - Multiply Big Numbers

---
+++ 03 - C# Advanced/05.Manual String Processing/08.Multiply Big Numbers/MultiplyBigNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Multiply_Big_Numbers
+{
+    public class MultiplyBigNumbers
+    {
+        public static void Main()
+        {
+            var firstNumber = new Stack<char>(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+
+            if (secondNumber == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var result = new StringBuilder();
+            var residue = 0;
+            while (firstNumber.Count > 0)
+            {
+                var sum = (int)char.GetNumericValue(firstNumber.Pop()) * secondNumber + residue;
+
+                residue = sum / 10;
+                result.Insert(0, sum % 10);
+            }
+
+            result.Insert(0, residue);
+
+            Console.WriteLine(result.ToString().TrimStart('0'));
+        }
+    }
+}
