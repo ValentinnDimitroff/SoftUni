4a547ca2ba2456a256ddeb433305818b43ebc206
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:02:06 +0300

    C# Advanced - Manual String Processing - Convert From Base N To Base 10

---
+++ 03 - C# Advanced/05.Manual String Processing/05.Convert From Base-N To Base-10/ConvertBaseNTo10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Convert_From_Base_N_To_Base_10
+{
+    public class ConvertBaseNTo10
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            var baseN = int.Parse(input[0]);
+            var number = input[1];
+
+            BigInteger result = 0;
+            BigInteger basePowered = 1;
+            for (int i = number.ToString().Length - 1; i >= 0; i--)
+            {
+                result += BigInteger.Multiply(int.Parse(number[i].ToString()), basePowered);
+                basePowered *= baseN;
+            }
+            
+            Console.WriteLine(result);
+        }
+    }
+}
