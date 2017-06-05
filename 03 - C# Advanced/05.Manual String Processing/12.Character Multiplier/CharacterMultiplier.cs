61a0acf84388ef1494a43af38e0767bc114922b5
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:04:27 +0300

    C# Advanced - Manual String Processing - Character Multiplier

---
+++ 03 - C# Advanced/05.Manual String Processing/12.Character Multiplier/CharacterMultiplier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Character_Multiplier
+{
+    public class CharacterMultiplier
+    {
+        public static void Main()
+        {
+            var inputStrings = Console.ReadLine()
+                .Split(' ');
+            
+            var firstString = new Queue<char>(inputStrings[0]);
+            var secondString = new Queue<char>(inputStrings[1]);
+            var totalSum = 0;
+
+            while (firstString.Count > 0 || secondString.Count > 0)
+            {
+                var sum = 1;
+                if (firstString.Count > 0) sum *= firstString.Dequeue();
+                if (secondString.Count > 0) sum *= secondString.Dequeue();
+
+                totalSum += sum;
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
