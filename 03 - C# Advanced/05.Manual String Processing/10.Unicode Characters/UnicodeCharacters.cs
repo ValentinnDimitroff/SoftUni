c2701f1b7345351ea82edc6a3859ea1d00a409cf
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:03:49 +0300

    C# Advanced - Manual String Processing - Unicode Character

---
+++ 03 - C# Advanced/05.Manual String Processing/10.Unicode Characters/UnicodeCharacters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Unicode_Characters
+{
+    public class UnicodeCharacters
+    {
+        public static void Main()
+        {
+            var inputText = Console.ReadLine();
+
+            var result = new StringBuilder();
+            foreach (var letter in inputText)
+            {
+                result.Append(@"\u" + $"{(int)letter:x4}");
+            }
+
+            Console.WriteLine(result.ToString()); 
+        }
+    }
+}
