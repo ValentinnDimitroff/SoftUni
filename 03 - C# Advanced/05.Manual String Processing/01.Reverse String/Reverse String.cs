61374c818f4345b54222ecb0c090bd3197256a68
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:00:47 +0300

    C# Advanced - Manual String Processing - Reverse String

---
+++ 03 - C# Advanced/05.Manual String Processing/01.Reverse String/Reverse String.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Reverse_String
+{
+    public class ReverseString
+    {
+        public static void Main()
+        {
+            //var s = new Stopwatch();
+            //s.Start();
+
+            //var inputStr = Console.ReadLine().ToCharArray().Reverse().ToArray();
+            //Console.WriteLine(string.Join("", inputStr));
+
+            //var stack = new Stack<char>(Console.ReadLine());
+            //Console.WriteLine(stack.ToString());
+
+            var inputStr = Console.ReadLine().ToCharArray();
+            Array.Reverse(inputStr);
+            Console.WriteLine(inputStr);
+
+            //s.Stop();
+            //Console.WriteLine(s.ElapsedTicks);
+        }
+    }
+}
