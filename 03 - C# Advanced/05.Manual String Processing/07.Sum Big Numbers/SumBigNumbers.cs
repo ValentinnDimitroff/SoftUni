8ab324db70742bc35b5038a07d438cc32ea01751
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:02:54 +0300

    C# Advanced - Manual String Processing - Sum Big Numbers

---
+++ 03 - C# Advanced/05.Manual String Processing/07.Sum Big Numbers/SumBigNumbers.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace _07.Sum_Big_Numbers
+{
+    public class SumBigNumbers
+    {
+        public static void Main()
+        {
+            var firstNumber = new Stack<char>(Console.ReadLine());
+            var secondNumber = new Stack<char>(Console.ReadLine());
+
+            var result = new StringBuilder();
+            var residue = 0;
+            while (firstNumber.Count > 0 || secondNumber.Count > 0)
+            {
+                var sum = residue;
+                if (firstNumber.Count > 0) sum += (int) char.GetNumericValue(firstNumber.Pop());
+                if (secondNumber.Count > 0) sum += (int) char.GetNumericValue(secondNumber.Pop());
+                
+                residue = sum / 10;
+                result.Insert(0, sum % 10);
+            }
+
+            result.Insert(0, residue);
+
+            Console.WriteLine(result.ToString().TrimStart('0'));
+        }
+    }
+}
