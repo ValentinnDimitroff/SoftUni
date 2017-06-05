3a252995c561f36a221520be5dc05c4791e006f9
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:01:25 +0300

    C# Advanced - Manual String Processing - Formatting Numbers

---
+++ 03 - C# Advanced/05.Manual String Processing/03.Formatting Numbers/Formatting Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Formatting_Numbers
+{
+    public class FormattingNumbers
+    {
+        public static void Main()
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(new []{' ', '\t', '\n', '\r'}, StringSplitOptions.RemoveEmptyEntries);
+            var intNum = int.Parse(inputNumbers[0]);
+            var firstFloatNum = double.Parse(inputNumbers[1]);
+            var secondFloatNum = double.Parse(inputNumbers[2]);
+
+            var firstColumn = intNum.ToString("X").PadRight(10, ' ');
+            var secondColumn = Convert.ToString(intNum, 2).PadLeft(10, '0').Substring(0, 10);
+            //var thirdColumn = firstFloatNum.ToString("F2").PadLeft(10, ' ');
+            //var fourthColumn = secondFloatNum.ToString("F3").PadRight(10, ' ');
+
+            Console.WriteLine($"|{firstColumn,10}|{secondColumn}|{firstFloatNum,10:f2}|{secondFloatNum,-10:f3}|");
+        }
+    }
+}
