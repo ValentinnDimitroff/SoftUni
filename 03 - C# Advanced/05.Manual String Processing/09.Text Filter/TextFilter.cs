a28cde8b065ecc3cdb0382a7dc1ab6831b1c81fe
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:03:30 +0300

    C# Advanced - Manual String Processing - Text Filter

---
+++ 03 - C# Advanced/05.Manual String Processing/09.Text Filter/TextFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Text_Filter
+{
+    public class TextFilter
+    {
+        public static void Main()
+        {
+            var bannedWords = Console.ReadLine()
+                .Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries);
+            var text = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
