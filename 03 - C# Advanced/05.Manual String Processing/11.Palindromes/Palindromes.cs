a2920b6927ff5c6cf9e8e8eba2c8510c2489a0af
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:04:04 +0300

    C# Advanced - Manual String Processing - Palindromes

---
+++ 03 - C# Advanced/05.Manual String Processing/11.Palindromes/Palindromes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Palindromes
+{
+    public class Palindromes
+    {
+        public static void Main()
+        {
+            var inputWords = Console.ReadLine()
+                .Split(new []{' ', '.', ',', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
+
+            var palindromes = new SortedSet<string>();
+
+            foreach (var word in inputWords)
+            {
+                var isPalindrome = true;
+
+                for (int i = 0; i < word.Length / 2; i++)
+                {
+                    if (!word[i].Equals(word[word.Length - i - 1]))
+                    {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                if (isPalindrome)
+                {
+                    palindromes.Add(word);
+                }
+            }
+
+            Console.WriteLine("[" + string.Join(", ", palindromes) + "]");
+        }
+    }
+}
