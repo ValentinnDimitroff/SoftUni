dd98bdec1518985c91cde1799f4ad48dd59f4ed8
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:02:34 +0300

    C# Advanced - Manual String Processing - Count Substring Occurrences

---
+++ 03 - C# Advanced/05.Manual String Processing/06.Count Substring Occurrences/CountSubstrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Count_Substring_Occurrences
+{
+    public class CountSubstrings
+    {
+        public static void Main()
+        {
+            var inputStr = Console.ReadLine();
+            var substirng = Console.ReadLine();
+
+            int index = 0, counter = 0;
+            while ((index = inputStr.IndexOf(substirng, index, StringComparison.OrdinalIgnoreCase)) != -1)
+            {
+                index++;
+                counter++;
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
