ac4bb2be5741d6d4f9801ffd9fc83a4bfa361976
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-05 21:01:00 +0300

    C# Advanced - Manual String Processing -  String Length

---
+++ 03 - C# Advanced/05.Manual String Processing/02.String Length/String Length.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.String_Length
+{
+    public class StringLength
+    {
+        public static void Main()
+        {
+            string inputStr = string.Empty;
+            while (inputStr.Length <= 20)
+            {
+                //char c = Console.ReadKey().KeyChar;
+                char c = (char) Console.Read();
+                if (c == '\r')
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                inputStr += c;
+            }
+            
+            inputStr = inputStr.PadRight(20, '*');
+            Console.WriteLine(inputStr);
+        }
+    }
+}
