3b20032c983cad8157a5c8487d64e867df916fb0
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-07 15:45:48 +0300

    C# Advanced - Manual String Processing - Parse URLs

---
+++ 03 - C# Advanced/05.Manual String Processing - Lab/02.ParseURLs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.ParseURLs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = Console.ReadLine();
+
+            var urlTokens = url.Split(new[] {"://"}, StringSplitOptions.RemoveEmptyEntries);
+            if (urlTokens.Length != 2 || urlTokens[1].IndexOf('/') == -1)
+            {
+                Console.WriteLine("Invalid URL");
+                return;
+            }
+            else
+            {
+                var protocol = urlTokens[0];
+                var serverEndIndex = urlTokens[1].IndexOf('/');
+                var server = urlTokens[1].Substring(0, serverEndIndex);
+                var resource = urlTokens[1].Substring(serverEndIndex + 1);
+
+                Console.WriteLine("Protocol = " + protocol);
+                Console.WriteLine("Server = " + server);
+                Console.WriteLine("Resources = " + resource);
+            }
+        }
+    }
+}
