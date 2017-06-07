3f5ca40b24488f4e7356b95dede96df296f9754d
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-07 15:46:16 +0300

    C# Advanced - Manual String Processing - Special Words

---
+++ 03 - C# Advanced/05.Manual String Processing - Lab/04.Special Words/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Special_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var specialWords = Console.ReadLine().Split();
+            var dict  = new Dictionary<string, int>();
+            foreach (var specialWord in specialWords)
+            {
+                dict.Add(specialWord, 0);   
+            }
+            var inputText = Console.ReadLine().Split(new []{ '(', ')', '[', ']', '<', '>', ',', '-', '!', '?', ' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var s in inputText)
+            {
+                if (dict.ContainsKey(s))
+                {
+                    dict[s]++;
+                }
+            }
+
+            foreach (var i in dict)
+            {
+                Console.WriteLine($"{i.Key} - {i.Value}");
+            }
+        }
+    }
+}
