ad48ec63da8726ea07aab6fe5500edda68dc8ac5
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-12 23:15:16 +0300

    C# Advanced - Functional Programming - Custom Min Function

---
+++ 03 - C# Advanced/07.Functional Programming/03.Custom Min Function/CustomMinFunc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class CustomMinFunc
+{
+    public static void Main()
+    {
+        var numbers = Console.ReadLine()
+            .Split(new []{' ', '\t', '\n', '\r'}, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Func<int[], int> getSmallest = n => n.Min();
+        Console.WriteLine(getSmallest(numbers));
+    }
+}
