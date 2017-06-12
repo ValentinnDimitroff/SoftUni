4c864cfc76a1000cef9cc2b329322551114e62d1
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-12 23:16:44 +0300

    C# Advanced - Functional Programming - Predicate for Names

---
+++ 03 - C# Advanced/07.Functional Programming/07.Predicate For Names/PredicateForNames.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    public class PredicateForNames
+    {
+        public static void Main()
+        {
+            var maxLength = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine()
+                .Split(' ').ToList();
+
+            var filteredNames = FilterNames(names, n => n.Length - maxLength <= 0);
+
+            foreach (var name in filteredNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        private static List<string> FilterNames(List<string> names, Func<string, bool> isShortEnough)
+        {
+            var result = new List<string>();
+
+            foreach (var name in names)
+            {
+                if (isShortEnough(name))
+                {
+                    result.Add(name);
+                }
+            }
+
+            return result;
+        }
+    }
