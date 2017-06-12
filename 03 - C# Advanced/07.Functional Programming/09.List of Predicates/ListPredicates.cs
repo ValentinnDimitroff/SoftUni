232f0448f16a6a46f40d603e4bde59289565f2e5
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-12 23:17:23 +0300

    C# Advanced - Functional Programming - List of Predicates

---
+++ 03 - C# Advanced/07.Functional Programming/09.List of Predicates/ListPredicates.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ListPredicates
+{
+    public static void Main()
+    {
+        int rangeBorder = int.Parse(Console.ReadLine());
+        var dividers = ReadNumbersSequence().ToArray();
+
+        if (rangeBorder < 0)
+            return;
+
+        var numbers = Enumerable.Range(1, rangeBorder).ToArray();
+        var filteredNumbers = FilterNumbers(numbers, dividers, (x, n) => x % n == 0);
+
+        Console.WriteLine(string.Join(" ", filteredNumbers));
+    }
+
+    private static List<int> FilterNumbers(int[] numbers, int[] dividers, Func<int, int, bool> filter)
+    {
+        var filteredNumbers = new List<int>();
+        foreach (var number in numbers)
+        {
+            var isValid = true;
+            foreach (var divider in dividers)
+            {
+                if (!filter(number, divider))
+                {
+                    isValid = false;
+                }
+            }
+            if (isValid) filteredNumbers.Add(number);
+        }
+
+        return filteredNumbers;
+    }
+
+    private static IEnumerable<int> ReadNumbersSequence()
+    {
+        var range = Console.ReadLine()
+            .Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse);
+
+        return range;
+    }
+}
