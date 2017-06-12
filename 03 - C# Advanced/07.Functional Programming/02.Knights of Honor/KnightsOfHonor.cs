fba90bd6d168c95151f4efb597e111997972ffd3
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-12 23:14:45 +0300

    C# Advanced - Functional Programming - Knights of Honor

---
+++ 03 - C# Advanced/07.Functional Programming/02.Knights of Honor/KnightsOfHonor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+public class KnightsOfHonor
+{
+    public static void Main()
+    {
+        Action<string, string> print = (pref, s) => Console.WriteLine(pref + " " + s);
+        var names = Console.ReadLine().Split();
+        PrintNamesWithPrep(names, "Sir", print);
+    }
+
+    private static void PrintNamesWithPrep(string[] names, string prefix, Action<string, string> action)
+    {
+        foreach (var name in names)
+        {
+            action(prefix,name);
+        }
+    }
+}
