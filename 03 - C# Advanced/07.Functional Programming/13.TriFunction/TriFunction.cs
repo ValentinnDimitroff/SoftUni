25b9044544b763365461d4db1a734818badc582c
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-14 09:53:38 +0300

    C# Advanced - Functional Programming - TriFunction

---
+++ 03 - C# Advanced/07.Functional Programming/13.TriFunction/TriFunction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.TriFunction
+{
+    public class TriFunction
+    {
+        public static void Main()
+        {
+            var charSum = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split(' ');
+
+            Func<string, int, bool> checkSum = (str, sum) =>
+            {
+                int localSum = 0;
+                foreach (var element in str)
+                {
+                    localSum += element;
+                }
+                return localSum >= sum;
+            } ;
+
+            var result = FindFirstMatch(names, charSum, checkSum);
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        private static string FindFirstMatch(string[] names, int charSum, Func<string, int, bool> checkSum)
+        {
+            foreach (var name in names)
+            {
+                if (checkSum(name, charSum))
+                {
+                    return name;
+                }    
+            }
+
+            return null;
+        }
+    }
+}
