c68100fc928f1037d8f824299cd9e17a9ebf56d2
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-18 23:33:17 +0300

    C# Advanced - LINQ - Sort Students

---
+++ 03 - C# Advanced/08.LINQ/04.Sort Students/SortStudents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Sort_Students
+{
+    public class SortStudents
+    {
+        public static void Main()
+        {
+            var group = new List<string[]>();
+            string student;
+
+            while ((student = Console.ReadLine()) != "END")
+                group.Add(student.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            group
+                .OrderBy(s => s[1])
+                .ThenByDescending(s => s[0])
+                .Select(s => $"{s[0]} {s[1]}")
+                .ToList()
+                .ForEach(s => Console.WriteLine(s));
+        }
+    }
+}
