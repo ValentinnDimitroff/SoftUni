d19b2d557e0ed2d68a839baf7d5d060d5e4cb367
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-18 23:30:24 +0300

    C# Advanced - LINQ - Students By First and Last Name

---
+++ 03 - C# Advanced/08.LINQ/02.Students By First And Last Name/StudentsByFirstLastName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Students_By_First_And_Last_Name
+{
+    public class StudentsByFirstLastName
+    {
+        public static void Main()
+        {
+            var group = new List<string[]>();
+            string student;
+
+            while ((student = Console.ReadLine()) != "END")
+                group.Add(student.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            group
+                .Where(s => s[0].CompareTo(s[1]) == -1)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x[0] + " " + x[1]));
+        }
+    }
+}
