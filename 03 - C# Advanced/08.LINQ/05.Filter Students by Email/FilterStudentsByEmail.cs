151e76071fd23efbe12dd71c54839bd7abc53676
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-18 23:35:24 +0300

    C# Advanced - LINQ - Filter Students by Email

---
+++ 03 - C# Advanced/08.LINQ/05.Filter Students by Email/FilterStudentsByEmail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Filter_Students_by_Email
+{
+    public class FilterStudentsByEmail
+    {
+        public static void Main()
+        {
+            var group = new List<string[]>();
+            var domain = "@gmail.com";
+            string student;
+
+            while ((student = Console.ReadLine()) != "END")
+                group.Add(student.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            group
+                .Where(s => s[2].Contains(domain))
+                .Select(s => $"{s[0]} {s[1]}")
+                .ToList()
+                .ForEach(s => Console.WriteLine(s));
+        }
+    }
+}
