495079669fe0b4331be86ba10fda766046f08d70
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-18 23:31:59 +0300

    C# Advanced - LINQ - Excellent Students

---
+++ 03 - C# Advanced/08.LINQ/07.Excellent Students/ExcellentStudents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Excellent_Students
+{
+    public class ExcellentStudents
+    {
+        public static void Main()
+        {
+            var group = new List<string[]>();
+            var domain = "@gmail.com";
+            string student;
+
+            while ((student = Console.ReadLine()) != "END")
+                group.Add(student.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            group
+                .Where(s => s.Any(x => x == "6"))
+                .Select(s => $"{s[0]} {s[1]}")
+                .ToList()
+                .ForEach(s => Console.WriteLine(s));
+        }
+    }
+}
