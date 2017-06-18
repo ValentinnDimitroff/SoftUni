cb2d1244da9c037edd175bba16707541bd509a37
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-18 23:36:10 +0300

    C# Advanced - LINQ -  Students by Age

---
+++ 03 - C# Advanced/08.LINQ/03.Students by Age/StudentsByAge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Students_by_Age
+{
+    public class StudentsByAge
+    {
+        public static void Main()
+        {
+            var group = new List<string[]>();
+            string student;
+
+            while ((student = Console.ReadLine()) != "END")
+                group.Add(student.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+            group
+                .Where(s => int.Parse(s[2]) >= 18 && int.Parse(s[2]) <= 24)
+                .Select(s => $"{s[0]} {s[1]} {s[2]}")
+                .ToList()
+                .ForEach(s => Console.WriteLine(s));
+        }
+    }
+}
