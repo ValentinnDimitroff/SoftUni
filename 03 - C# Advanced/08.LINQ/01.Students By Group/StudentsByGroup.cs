bbd8cf7db18c4bbae853cbd74348993cdea27d52
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-18 23:43:13 +0300

    C# Advanced - LINQ - Students by Group

---
+++ 03 - C# Advanced/08.LINQ/01.Students By Group/StudentsByGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Students_By_Group
+{
+    public class StudentsByGroup
+    {
+        public static void Main()
+        {
+            var group = new List<string[]>();
+            string student;
+
+            while ((student = Console.ReadLine()) != "END")
+                group.Add(student.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries));
+
+            group
+                .Where(s => s.Last() == "2")
+                .OrderBy(a => a[0])
+                .ToList()
+                .ForEach(x => Console.WriteLine(x[0] + " " + x[1]));
+        }
+    }
+}
