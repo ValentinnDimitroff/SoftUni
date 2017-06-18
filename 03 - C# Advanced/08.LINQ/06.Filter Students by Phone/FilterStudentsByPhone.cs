d95b8b7f5d4c3fec49eee252afab075a11473266
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-18 23:32:24 +0300

    C# Advanced - LINQ - Filter Students by Phone

---
+++ 03 - C# Advanced/08.LINQ/06.Filter Students by Phone/FilterStudentsByPhone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class FilterStudentsByPhone
+{
+    public static void Main()
+    {
+        var group = new List<string[]>();
+        string student;
+
+        while ((student = Console.ReadLine()) != "END")
+            group.Add(student.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+        group
+            .Where(s => s[2].StartsWith("02") || s[2].StartsWith("+3592"))
+            .Select(s => $"{s[0]} {s[1]}")
+            .ToList()
+            .ForEach(s => Console.WriteLine(s));
+    }
+}
