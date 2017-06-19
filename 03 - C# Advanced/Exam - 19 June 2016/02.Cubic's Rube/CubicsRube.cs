429ad2c6835ec0a6b8b1f042596f45983eaf8429
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-19 23:58:53 +0300

    C# Advanced - 19 June 2016 - Cubic's Rube

---
+++ 03 - C# Advanced/Exam - 19 June 2016/02.Cubic's Rube/CubicsRube.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CubicsRube
+{
+    public static void Main()
+    {
+        var dimension = int.Parse(Console.ReadLine());
+        
+        long sumOfParticles = 0;
+        var cellsChanged = 0;
+
+        var line = Console.ReadLine();
+        while (line != "Analyze")
+        {
+            var tokens = line
+                .Split(' ')
+                .Select(long.Parse)
+                .ToArray();
+            var pointsInside = tokens
+                .Take(3)
+                .Where(pt => pt < dimension && pt >= 0)
+                .ToList();
+            if (pointsInside.Count == 3 && tokens[3] != 0)
+            {
+                sumOfParticles += tokens[3];
+                cellsChanged++;
+            }
+
+            line = Console.ReadLine();
+        }
+
+        Console.WriteLine(sumOfParticles);
+        var res = Math.Pow(dimension, 3) - cellsChanged;
+        Console.WriteLine(res);
+    }
+}
