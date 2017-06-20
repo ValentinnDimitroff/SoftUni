d7244ef7a7abef223d5e17133b7ee863f263eef2
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-20 11:33:47 +0300

    C# Advanced - Regex - Replace a Tag

---
+++ 03 - C# Advanced/06.Regex/04.Replace a Tag/ReplaceATag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class ReplaceATag
+{
+    public static void Main()
+    {
+        var html = new StringBuilder();
+        string inputLine;
+        while ((inputLine = Console.ReadLine()) != "end")
+        {
+            html.Append(inputLine).Append(Environment.NewLine);
+        }
+
+        var htmlText = html.ToString();
+
+        var endTagPattern = @"</a>";
+        var beginningTagPattern = "(<a href=([^>]*)>)";
+
+        var editedHtml = Regex.Replace(htmlText, beginningTagPattern, "[URL href=$2]");
+        editedHtml = Regex.Replace(editedHtml, endTagPattern, "[/URL]");
+        Console.WriteLine(editedHtml);
+    }
+}
