d47e50d77b7ad3f102074aa108e58038b047cd88
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-20 11:14:48 +0300

    C# Advanced - Regex - Match Phone Number

---
+++ 03 - C# Advanced/06.Regex/02.Match Phone Number/MatchPhoneNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class MatchPhoneNumbers
+{
+    public static void Main()
+    {
+        var pattern = @"^[\+]359(?<del> |-)[2](\k<del>)[\d]{3}(\k<del>)[\d]{4}\b";
+
+        string inputLine;
+        while ((inputLine = Console.ReadLine()) != "end")
+        {
+            if (inputLine != "" && Regex.IsMatch(inputLine, pattern))
+            {
+                Console.WriteLine(inputLine);
+            }
+        }
+    }
+}
