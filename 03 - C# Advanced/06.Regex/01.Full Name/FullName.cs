ad9d02310a90fc44bd8b181939c99f0a8500c682
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-20 09:53:55 +0300

    C# Advanced - Regex - Full Name

---
+++ 03 - C# Advanced/06.Regex/01.Full Name/FullName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.Full_Name
+{
+    public class FullName
+    {
+        public static void Main()
+        {
+            var pattern = @"(^[A-Z][a-z]+ [A-Z][a-z]+)";
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "end")
+            {
+                if (inputLine != "" && Regex.IsMatch(inputLine, pattern))
+                {
+                    Console.WriteLine(inputLine); 
+                }
+            }
+        }
+    }
+}
