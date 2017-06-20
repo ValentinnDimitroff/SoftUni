9fb8fee86c15198c6209d00faa841b4c9f05624e
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-20 11:14:18 +0300

    C# Advanced - Regex - Series of Letters

---
+++ 03 - C# Advanced/06.Regex/03.Series of Letters/SeriesOfLetters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public class SeriesOfLetters
+{
+    public static void Main()
+    {
+        var inputStr = Console.ReadLine();
+
+        var reg = new Regex(@"([\w])(\1+)");
+        var result = reg.Replace(inputStr, "$1");
+        Console.WriteLine(result);
+    }
+}
