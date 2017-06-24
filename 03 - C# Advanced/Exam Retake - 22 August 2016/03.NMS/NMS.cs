03260647c731ffdb3ed1f5dd65c054c82237e2d8
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-24 15:58:40 +0300

    C# Advanced - Exam - 22 August 2016 - NMS

---
+++ 03 - C# Advanced/Exam Retake - 22 August 2016/03.NMS/NMS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+public class NMS
+{
+    public static void Main()
+    {
+        var message = new StringBuilder();
+
+        string input;
+        while ((input = Console.ReadLine()) != "---NMS SEND---")
+            message.Append(input);
+        var delimiter = Console.ReadLine();
+
+        var output = new StringBuilder();
+        output.Append(message[0]);
+        for (int i = 1; i < message.Length; i++)
+        {
+            if (char.ToLower(message[i]) >= char.ToLower(message[i - 1]))
+            {
+                output.Append(message[i]);
+            }
+            else
+            {
+                output.Append(delimiter);
+                output.Append(message[i]);
+            }
+        }
+
+        Console.WriteLine(output);
+    }
+}
