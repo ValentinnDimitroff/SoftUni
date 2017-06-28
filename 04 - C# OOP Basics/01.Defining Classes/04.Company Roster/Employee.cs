188df9c61d1584b88163348b57306258db3da056
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-28 14:34:56 +0300

    C# OOP Basics - Defining Classes - Company Roster

---
+++ 04 - C# OOP Basics/01.Defining Classes/04.Company Roster/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace CompanyRoster
+{
+    public class Employee
+    {
+        public string name;
+        public decimal salary;
+        public string position;
+        public string department;
+        public string email = "n/a";
+        public int age = -1;
+
+        public Employee(string name, decimal salary, string position, string department)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.position = position;
+            this.department = department;
+        }
+    }
+}
---
+++ 04 - C# OOP Basics/01.Defining Classes/04.Company Roster/StartUp.cs
@@ -0,0 +1,62 @@
+﻿namespace CompanyRoster
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfEmployees = int.Parse(Console.ReadLine());
+            var employees = new List<Employee>();
+
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                var employeeInfo = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var employee = new Employee(
+                    employeeInfo[0],
+                    decimal.Parse(employeeInfo[1]),
+                    employeeInfo[2],
+                    employeeInfo[3]);
+
+                if (employeeInfo.Length > 4)
+                {
+                    var ageOrEmail = employeeInfo[4];
+                    if (ageOrEmail.IndexOf("@") > 0)
+                    {
+                        employee.email = ageOrEmail;
+                    }
+                    else
+                    {
+                        employee.age = int.Parse(ageOrEmail);
+                    }
+                }
+
+                if (employeeInfo.Length > 5)
+                {
+                    employee.age = int.Parse(employeeInfo[5]);
+                }
+
+                employees.Add(employee);
+            }
+
+            var result = employees
+                .GroupBy(e => e.department)
+                .Select(e => new
+                {
+                    Department = e.Key,
+                    AverageSalary = e.Average(emp => emp.salary),
+                    Employees = e.OrderByDescending(emp => emp.salary)
+                })
+                .OrderByDescending(gr => gr.AverageSalary)
+                .FirstOrDefault();
+
+
+            Console.WriteLine($"Highest Average Salary: {result.Department}");
+            foreach (var employee in result.Employees)
+            {
+                Console.WriteLine($"{employee.name} {employee.salary:f2} {employee.email} {employee.age}");
+            }
+        }
+    }
+}
