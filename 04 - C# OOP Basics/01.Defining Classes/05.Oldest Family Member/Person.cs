60173ba0461d392364c6493ce3a064e12c6f4758
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-28 14:16:26 +0300

    C# OOP Basics - Defining Classes - Oldest Family Member

---
+++ 04 - C# OOP Basics/01.Defining Classes/05.Oldest Family Member/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace OldestFamilyMember
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
---
+++ 04 - C# OOP Basics/01.Defining Classes/05.Oldest Family Member/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace OldestFamilyMember
+{
+    using System;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            MethodInfo oldestMemberMethod = typeof(Family).GetMethod("GetOldestMember");
+            MethodInfo addMemberMethod = typeof(Family).GetMethod("AddMember");
+            if (oldestMemberMethod == null || addMemberMethod == null)
+            {
+                throw new Exception();
+            }
+
+            var family = new Family();
+            var numberOfPeople = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                var memberInfo = Console.ReadLine().Split(' ');
+                family.AddMember(new Person(memberInfo[0], int.Parse(memberInfo[1])));
+            }
+
+            var oldestMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestMember.name} {oldestMember.age}");
+        }
+    }
+}
