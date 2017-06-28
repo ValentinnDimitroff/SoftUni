669f74fad2de3d90317a15d3facd4055d9c9e1e0
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-28 14:20:59 +0300

    C# OOP Basics - Defining Classes - Opinion Poll

---
+++ 04 - C# OOP Basics/01.Defining Classes/03.Opinion Poll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace OpinionPoll
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var allPeople = new List<Person>();
+            var numberOfPeople = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                var personInputInfo = Console.ReadLine().Split(' ');
+                var person = new Person(
+                    personInputInfo[0],
+                    int.Parse(personInputInfo[1]));
+                allPeople.Add(person);
+            }
+
+            allPeople
+                .Where(p => p.age > 30)
+                .OrderBy(p => p.name)
+                .ToList()
+                .ForEach(p => Console.WriteLine($"{p.name} - {p.age}"));
+        }
+    }
+}
