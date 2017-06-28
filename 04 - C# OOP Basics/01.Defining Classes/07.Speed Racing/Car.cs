4ea5f92b35d167634d6e96d8ab5b681b4ab967d8
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-28 16:34:15 +0300

    C# OOP Basics - Defining Classes - Speed Racing

---
+++ 04 - C# OOP Basics/01.Defining Classes/07.Speed Racing/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumption;
+        private int distanceTraveled;
+
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumption = fuelConsumption;
+            this.distanceTraveled = 0;
+        }
+
+        public void Drive(int kilometers)
+        {
+            double neededFuel = fuelConsumption * kilometers;
+            if (neededFuel > this.fuelAmount)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                this.fuelAmount -= neededFuel;
+                this.distanceTraveled += kilometers;
+            }
+        }
+
+        public string PrintCarInfo()
+        {
+            return $"{this.model} {this.fuelAmount:f2} {this.distanceTraveled}";
+        }
+    }
+}
---
+++ 04 - C# OOP Basics/01.Defining Classes/07.Speed Racing/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfCars = int.Parse(Console.ReadLine());
+            var allCars = new Dictionary<string, Car>();
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                var carInfo = Console.ReadLine().Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+                var car = new Car(carInfo[0], double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+                allCars[carInfo[0]] = car;
+            }
+            string driveLine;
+            while ((driveLine = Console.ReadLine()) != "End")
+            {
+                var driveInfo = driveLine.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                var model = driveInfo[1];
+                var distance = int.Parse(driveInfo[2]);
+                allCars[model].Drive(distance);
+            }
+
+            foreach (var car in allCars)
+            {
+                Console.WriteLine(car.Value.PrintCarInfo());
+            }
+        }
+    }
+}
