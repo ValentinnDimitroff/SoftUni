eba6f4adbe6f2749f9d454a8e58ada3dd66fa3c6
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-28 15:23:24 +0300

    C# OOP Basics - Defining Classes - Date Modifier

---
+++ 04 - C# OOP Basics/01.Defining Classes/06.Date Modifier/DateModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace DateMod
+{
+    public class DateModifier
+    {
+        private int daysBetween;
+
+        public int DaysBetween
+        {
+            get { return this.daysBetween; }
+            set { this.daysBetween = value; }
+        }
+
+        public void CalculateDifference(string firstDate, string secondDate)
+        {
+            var dateOne = Convert.ToDateTime(firstDate);
+            var dateTwo = Convert.ToDateTime(secondDate);
+
+            this.DaysBetween = (int)Math.Abs((dateOne - dateTwo).TotalDays);
+        }
+    }
+}
---
+++ 04 - C# OOP Basics/01.Defining Classes/06.Date Modifier/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace DateMod
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var fistDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            var dateMod = new DateModifier();
+            dateMod.CalculateDifference(fistDate, secondDate);
+
+            Console.WriteLine(dateMod.DaysBetween);
+        }
+    }
+}
