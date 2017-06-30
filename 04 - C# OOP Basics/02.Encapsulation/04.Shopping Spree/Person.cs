69765865e2e146b77398e56a0b4d9623e806a2b8
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-30 13:29:42 +0300

    C# OOP Basics - Encapsulation - Shopping Spree

---
+++ 04 - C# OOP Basics/02.Encapsulation/04.Shopping Spree/Person.cs
@@ -0,0 +1,65 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bag = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"{nameof(Name)} cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        private decimal Money
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException($"{nameof(Money)} cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public void BuyProduct(Product product)
+        {
+            if (this.money - product.Price < 0)
+            {
+                throw new InvalidOperationException($"{this.Name} can't afford {product.Name}");
+            }
+
+            this.money -= product.Price;
+            this.bag.Add(product);
+        }
+
+        public IList<Product> GetProducts()
+        {
+            return this.bag.AsReadOnly();
+        }
+    }
+}
---
+++ 04 - C# OOP Basics/02.Encapsulation/04.Shopping Spree/Startup.cs
@@ -0,0 +1,68 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Startup
+    {
+        public static void Main()
+        {
+            var people = new List<Person>();
+            var products = new List<Product>();
+
+            var allPeople = Console.ReadLine()
+                .Split(new [] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            var allProducts = Console.ReadLine()
+                .Split(new [] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            try
+            {
+                foreach (var currentPerson in allPeople)
+                {
+                    var tokens = currentPerson.Split('=');
+                    var person = new Person(tokens[0], decimal.Parse(tokens[1]));
+                    people.Add(person);
+                }
+
+                foreach (var currentProduct in allProducts)
+                {
+                    var tokens = currentProduct.Split('=');
+                    var product = new Product(tokens[0], decimal.Parse(tokens[1]));
+                    products.Add(product);
+                }
+
+                string purchases;
+                while ((purchases = Console.ReadLine()) != "END")
+                {
+                    var tokens = purchases
+                        .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    var personName = tokens[0];
+                    var productName = tokens[1];
+
+                    try
+                    {
+                        var person = people.FirstOrDefault(pers => pers.Name == personName);
+                        var product = products.FirstOrDefault(prod => prod.Name == productName);
+                        person.BuyProduct(product);
+                        Console.WriteLine($"{personName} bought {productName}");
+                    }
+                    catch (InvalidOperationException ioe)
+                    {
+                        Console.WriteLine(ioe.Message);
+                    }
+                }
+
+                foreach (var person in people)
+                {
+                    var boughtProducts = string.Join(", ", person.GetProducts().Select(prod => prod.Name).ToList());
+                    var result = boughtProducts.Any() ? boughtProducts : "Nothing bought";
+                    Console.WriteLine($"{person.Name} - {result}");
+                }
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
