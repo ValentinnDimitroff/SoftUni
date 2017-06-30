4046cf0f799205724562e9f84790da2e267c78f3
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-06-30 13:27:26 +0300

    C# OOP Basics - Encapsulation - Class Box Data Validation

---
+++ 04 - C# OOP Basics/02.Encapsulation/02.ClassBoxDataValidation/Box.cs
@@ -0,0 +1,69 @@
+﻿namespace ClassBoxDataValidation
+{
+    using System;
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        private double Length
+        {
+            set
+            {
+                if (value <= 0 )
+                {
+                    throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                }
+                this.length = value;
+            }
+        }
+
+        private double Width
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+
+        private double Height
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            var surface = 2 * this.height * this.length + 2 * this.height * this.width + 2 * this.width * this.length;
+
+            return surface;
+        }
+        public double GetLateralSurfaceArea()
+        {
+            return 2 * this.length * this.height + 2 * this.width * this.height;
+        }
+
+        public double GetVolume()
+        {
+            return this.height * this.width * this.length;
+        }
+    }
+}
