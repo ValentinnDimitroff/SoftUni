9a76f69df24cde6ab878d3b519aa6bac8e267019
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-02 21:36:12 +0300

    C# OOP Basics - Defining Classes - Pokemon Trainer

---
+++ 04 - C# OOP Basics/01.Defining Classes/11.Pokemon Trainer/Startup.cs
@@ -0,0 +1,68 @@
+﻿namespace PokemonTrainer
+{
+    using System;
+    using System.CodeDom;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.ConstrainedExecution;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var trainers = new Dictionary<string, Trainer>();
+
+            CollectTrainersAndCatchPokemons(trainers);
+            GiveBadgesOrRemovePokemons(trainers);
+            PrintAllTrainers(trainers);
+        }
+
+        private static void PrintAllTrainers(Dictionary<string, Trainer> trainers)
+        {
+            foreach (var trainer in trainers.OrderByDescending(t => t.Value.NumberOfBadges))
+            {
+                Console.WriteLine(trainer.Value);
+            }
+        }
+
+        private static void GiveBadgesOrRemovePokemons(Dictionary<string, Trainer> trainers)
+        {
+            string inputElement;
+            while ((inputElement = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Value.Pokemons.Any(p => p.Element == inputElement))
+                    {
+                        trainer.Value.NumberOfBadges++;
+                    }
+                    else
+                    {
+                        trainer.Value.PokemonsTakeDamage();
+                    }
+                }
+            }
+        }
+
+        private static void CollectTrainersAndCatchPokemons(Dictionary<string, Trainer> trainers)
+        {
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "Tournament")
+            {
+                var tokens = inputLine.Split(' ');
+                var trainerName = tokens[0];
+                var pokemonName = tokens[1];
+                var pokemonElement = tokens[2];
+                var pokemonHealth = int.Parse(tokens[3]);
+
+                var pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                if (!trainers.ContainsKey(trainerName))
+                {
+                    trainers[trainerName] = new Trainer(trainerName);
+                }
+                
+                trainers[trainerName].AddPokemon(pokemon);
+            }
+        }
+    }
+}
---
+++ 04 - C# OOP Basics/01.Defining Classes/11.Pokemon Trainer/Trainer.cs
@@ -0,0 +1,51 @@
+﻿namespace PokemonTrainer
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Trainer
+    {
+        private List<Pokemon> pokemons;
+
+        public Trainer(string name)
+        {
+            this.Name = name;
+            this.NumberOfBadges = 0;
+            this.pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+
+        public int NumberOfBadges { get; set; }
+
+        public IReadOnlyList<Pokemon> Pokemons
+        {
+            get { return this.pokemons.AsReadOnly(); }
+        }
+
+        public void PokemonsTakeDamage()
+        {
+            for (int i = this.pokemons.Count - 1; i >= 0; i--)
+            {
+                if (this.pokemons[i].Health <= 10)
+                {
+                    this.pokemons.RemoveAt(i);
+                }
+                else
+                {
+                    pokemons[i].Health -= 10;
+                }
+            }
+        }
+
+        public void AddPokemon(Pokemon pokemon)
+        {
+            this.pokemons.Add(pokemon);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.NumberOfBadges} {this.pokemons.Count}";
+        }
+    }
+}
