b3b9d10435d532308d87f4382fc856dc3f1452ee
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-02 21:36:40 +0300

    C# OOP Basics - Defining Classes - Family Tree

---
+++ 04 - C# OOP Basics/01.Defining Classes/13.Family Tree/Person.cs
@@ -0,0 +1,50 @@
+﻿namespace FamilyTree
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Person
+    {
+        private List<Person> children;
+
+        public Person()
+        {
+            this.children = new List<Person>();
+        }
+
+        public Person(string nameOrDate) : this()
+        {
+            if (nameOrDate.Contains("/"))
+            {
+                this.BirthDate = nameOrDate;
+            }
+            else
+            {
+                this.Name = nameOrDate;
+            }
+        }
+
+        public Person(string name, string date) : this()
+        {
+            this.Name = name;
+            this.BirthDate = date;
+        } 
+
+        public string BirthDate { get; set; }
+        public string Name { get; set; }
+
+        public IReadOnlyList<Person> Children => this.children.AsReadOnly(); 
+
+        public void AddChild(Person child) => this.children.Add(child);
+
+        public void AddChildInfo(Person child)
+        {
+            if (this.children.SingleOrDefault(c => c.Name == child.Name) != null)
+                this.children.SingleOrDefault(c => c.Name == child.Name).BirthDate = child.BirthDate;
+            else if (this.children.SingleOrDefault(c => c.BirthDate == child.BirthDate) != null)
+                this.children.SingleOrDefault(c => c.BirthDate == child.BirthDate).Name = child.Name;
+        }
+
+        public Person FindChild(Person child) => this.children.SingleOrDefault(c => c.Name == child.Name);
+    }
+}
---
+++ 04 - C# OOP Basics/01.Defining Classes/13.Family Tree/Program.cs
@@ -0,0 +1,121 @@
+﻿namespace FamilyTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var allPeople = new List<Person>();
+            var personSearched = Console.ReadLine();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input.Contains("-"))
+                {
+                    var tokens = input
+                        .Split(new[] {'-'}, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(x => x.Trim())
+                        .ToArray();
+
+                    var parentParam = tokens[0];
+                    var childParam = tokens[1];
+
+                    var parent = new Person(parentParam);
+                    var child = new Person(childParam);
+
+                    AddParentIfMissing(allPeople, parent);
+
+                    if (parentParam.Contains("/"))
+                        allPeople.SingleOrDefault(p => p.BirthDate == parentParam).AddChild(child);
+                    else
+                        allPeople.SingleOrDefault(p => p.Name == parentParam).AddChild(child);
+                }
+                else
+                {
+                    var tokens = input
+                        .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                    var name = $"{tokens[0]} {tokens[1]}";
+                    var date = tokens[2];
+                    CheckForMissingData(allPeople, name, date);
+                }
+            }
+
+            PrintParentsAndChildren(allPeople, personSearched);
+        }
+
+        private static void PrintParentsAndChildren(List<Person> allPeople, string personSearched)
+        {
+            var result = new StringBuilder();
+            Person startPerson = null;
+            if (personSearched.Contains("/"))
+            {
+                startPerson = allPeople.FirstOrDefault(p => p.BirthDate == personSearched);
+            }
+            else
+            {
+                startPerson = allPeople.FirstOrDefault(p => p.Name == personSearched);
+            }
+
+            result.AppendLine($"{startPerson.Name} {startPerson.BirthDate}");
+
+            result.AppendLine("Parents:");
+            foreach (var person in allPeople.Where(p => p.FindChild(startPerson) != null))
+            {
+                result.AppendLine($"{person.Name} {person.BirthDate}");
+            }
+
+            result.AppendLine("Children:");
+            foreach (var child in startPerson.Children)
+            {
+                result.AppendLine($"{child.Name} {child.BirthDate}");
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static void AddParentIfMissing(List<Person> allPeople, Person parent)
+        {
+            if (parent.Name != null && allPeople.SingleOrDefault(c => c.Name == parent.Name) != null)
+            {
+                return;
+            }
+
+            if (parent.BirthDate != null && allPeople.SingleOrDefault(c => c.BirthDate == parent.BirthDate) !=
+                     null)
+            {
+                return;
+            }
+
+            allPeople.Add(parent);
+        }
+
+        private static void CheckForMissingData(List<Person> allPeople, string name, string date)
+        {
+            var added = false;
+            foreach (var person in allPeople)
+            {
+                if (person.Name == name)
+                {
+                    person.BirthDate = date;
+                    added = true;
+                }
+                else if (person.BirthDate == date)
+                {
+                    person.Name = name;
+                    added = true;
+                }
+
+                person.AddChildInfo(new Person(name, date));
+            }
+
+            if (!added)
+                allPeople.Add(new Person(name, date));
+        }
+    }
+}
