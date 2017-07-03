ca45cde944ec082420ede6342120ee4fc9cada40
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-03 23:08:52 +0300

    C# OOP Basics - Inheritance - Mankind

---
+++ 04 - C# OOP Basics/03.Inheritance/03.Mankind/Student.cs
@@ -0,0 +1,40 @@
+﻿namespace Mankind
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class Student : Human
+    {
+        private string facultyNumber;
+
+        public Student(string firstName, string secondName, string facultyNumber) : base(firstName, secondName)
+        {
+            this.FacultyNumber = facultyNumber;
+        }
+
+        public string FacultyNumber
+        {
+            get { return this.facultyNumber; }
+            set
+            {
+                if (!Regex.IsMatch(value, "^([A-Za-z\\d]{5,10})$"))
+                {
+                    throw new ArgumentException("Invalid faculty number!");
+                }
+
+                this.facultyNumber = value;
+            }
+        }
+
+       public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(base.ToString())
+                .Append("Faculty number: ").Append(this.FacultyNumber)
+                .AppendLine();
+            
+            return sb.ToString();
+        }
+    }
+}
---
+++ 04 - C# OOP Basics/03.Inheritance/03.Mankind/Worker.cs
@@ -0,0 +1,56 @@
+﻿namespace Mankind
+{
+    using System;
+    using System.Text;
+
+    public class Worker : Human
+    {
+        private decimal weekSalary;
+        private double workHoursPerDay;
+        private string _secondName;
+
+        public Worker(string firstName, string secondName, decimal weekSalary, double workHoursPerDay) : base(firstName, secondName)
+        {
+            this.WeekSalary = weekSalary;
+            this.WorkHoursPerDay = workHoursPerDay;
+        }
+
+        public double WorkHoursPerDay
+        {
+            get { return this.workHoursPerDay; }
+            set
+            {
+                if (value < 1 || value > 12)
+                {
+                    throw new ArgumentException("Expected value mismatch! Argument: workHoursPerDay");
+                }   
+                this.workHoursPerDay = value;
+            }
+        }
+
+        public decimal WeekSalary
+        {
+            get { return this.weekSalary; }
+            set
+            {
+                if (value < 10)
+                {
+                    throw new ArgumentException("Expected value mismatch! Argument: weekSalary");
+                }
+
+                this.weekSalary = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(base.ToString())
+                .Append("Week Salary: ").AppendLine($"{this.WeekSalary:f2}")
+                .Append("Hours per day: ").AppendLine($"{this.WorkHoursPerDay:f2}")
+                .Append("Salary per hour: ").AppendLine($"{this.WeekSalary / ((decimal)this.WorkHoursPerDay * 5m):f2}");
+
+            return sb.ToString();
+        }
+    }
+}
