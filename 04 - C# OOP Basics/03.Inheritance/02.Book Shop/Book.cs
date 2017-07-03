34f4f41354927b96c9dc5ef21f56b98019adc9b9
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-03 23:07:39 +0300

    C# OOP Basics - Inheritance - Book Shop

---
+++ 04 - C# OOP Basics/03.Inheritance/02.Book Shop/Book.cs
@@ -0,0 +1,77 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Text;
+
+    public class Book
+    {
+        private string title;
+        private string author;
+        private decimal price;
+
+        public Book(string author, string title, decimal price)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Price = price;
+        }
+
+        protected virtual decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+                this.price = value;
+            }
+        }
+        
+        protected string Author
+        {
+            get { return this.author; }
+            set
+            {
+                this.ValidateAuthorName(value);
+                this.author = value;
+            }
+        }
+        
+        protected string Title
+        {
+            get { return this.title; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+                
+                this.title = value;
+            }
+        }
+
+        private void ValidateAuthorName(string name)
+        {
+            var names = name.Split(' ');
+            if (names.Length > 1 && char.IsDigit(names[1][0]))
+            {
+                throw new ArgumentException("Author not valid!");
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("Type: ").AppendLine(this.GetType().Name)
+                .Append("Title: ").AppendLine(this.Title)
+                .Append("Author: ").AppendLine(this.Author)
+                .Append("Price: ").Append($"{this.Price:f1}")
+                .AppendLine();
+
+            return sb.ToString();
+        }
+    }
+}
