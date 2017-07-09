20343111c3da7e53a5a29baa6aa0336f59305f1f
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-10 00:56:54 +0300

    C# OOP Basics - Polymorphism -  *Refactoring

--- 04 - C# OOP Basics/04.Polymorphism/03.Wild Farm/Startup.cs
+++ 04 - C# OOP Basics/04.Polymorphism/03.Wild Farm/Startup.cs
@@ -9,53 +9,51 @@
             string inputLine;
             while ((inputLine = Console.ReadLine()) != "End")
             {
-               
-                    var animalInfo = inputLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    var animalType = animalInfo[0];
-                    var animalName = animalInfo[1];
-                    var animalWeight = double.Parse(animalInfo[2]);
-                    var animalRegion = animalInfo[3];
+                var animalInfo = inputLine.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                var animalType = animalInfo[0];
+                var animalName = animalInfo[1];
+                var animalWeight = double.Parse(animalInfo[2]);
+                var animalRegion = animalInfo[3];
 
-                    var foodInfo = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-                    Food food;
-                    if (foodInfo[0] == "Vegetable")
-                        food = new Vegetable(int.Parse(foodInfo[1]));
-                    else 
-                        food = new Meat(int.Parse(foodInfo[1]));
+                var foodInfo = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                Food food;
+                if (foodInfo[0] == "Vegetable")
+                    food = new Vegetable(int.Parse(foodInfo[1]));
+                else
+                    food = new Meat(int.Parse(foodInfo[1]));
 
-                    Animal animal;
-                    switch (animalType)
-                    {
-                        case "Mouse":
-                            animal = new Mouse(animalName, animalType, animalWeight, animalRegion);
-                            break;
-                        case "Zebra":
-                            animal = new Zebra(animalName, animalType, animalWeight, animalRegion);
-                            break;
-                        case "Cat":
-                            animal = new Cat(animalName, animalType, animalWeight, animalRegion, animalInfo[4]);
-                            break;
-                        case "Tiger":
-                            animal = new Tiger(animalName, animalType, animalWeight, animalRegion);
-                            break;
-                        default:
-                            animal = null;
-                            break;
-                    }
+                Animal animal;
+                switch (animalType)
+                {
+                    case "Mouse":
+                        animal = new Mouse(animalName, animalType, animalWeight, animalRegion);
+                        break;
+                    case "Zebra":
+                        animal = new Zebra(animalName, animalType, animalWeight, animalRegion);
+                        break;
+                    case "Cat":
+                        animal = new Cat(animalName, animalType, animalWeight, animalRegion, animalInfo[4]);
+                        break;
+                    case "Tiger":
+                        animal = new Tiger(animalName, animalType, animalWeight, animalRegion);
+                        break;
+                    default:
+                        animal = null;
+                        break;
+                }
 
-                    Console.WriteLine(animal.MakeSound());
+                Console.WriteLine(animal.MakeSound());
 
-                    try
-                    {
-                        animal.Eat(food);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                    
-                    Console.WriteLine(animal);
-               
+                try
+                {
+                    animal.Eat(food);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine(animal);
             }
         }
     }
