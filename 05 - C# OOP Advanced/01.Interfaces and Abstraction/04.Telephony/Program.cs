8ad5b1e5a6d7bd59700f86ca2f7f34945f7779fe
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-18 22:37:00 +0300

    C# OOP Advanced - Interfaces and Abstraction - Telephony

---
+++ 05 - C# OOP Advanced/01.Interfaces and Abstraction/04.Telephony/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _04.Telephony
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var smartPhone = new Smartphone();
+            var phoneNumbers = Console.ReadLine().Split(' ');
+            var webSites = Console.ReadLine().Split(' ');
+
+            foreach (var phoneNumber in phoneNumbers)
+            {
+                var result = smartPhone.Call(phoneNumber);
+                Console.WriteLine(result);
+            }
+
+            foreach (var webSite in webSites)
+            {
+                var result = smartPhone.Browse(webSite);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/01.Interfaces and Abstraction/04.Telephony/Smartphone.cs
@@ -0,0 +1,47 @@
+﻿namespace _04.Telephony
+{
+    using System.Linq;
+
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public string Call(string number)
+        {
+            if (!this.ValidatePhoneNumber(number))
+            {
+                return "Invalid number!";
+            }
+
+            return $"Calling... {number}";
+        }
+
+        public string Browse(string url)
+        {
+            if (!this.ValidateURL(url))
+            {
+                return "Invalid URL!";
+            }
+
+            return $"Browsing: {url}!";
+        }
+
+        private bool ValidatePhoneNumber(string number)
+        {
+            if (!number.Any(char.IsDigit))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidateURL(string url)
+        {
+            if (url.Any(char.IsDigit))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
