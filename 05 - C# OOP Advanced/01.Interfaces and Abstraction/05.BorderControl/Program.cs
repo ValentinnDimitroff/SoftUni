ff79969f1beaf9b71e9210f8d66c4a70d982cc1f
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-18 22:39:21 +0300

    C# OOP Advanced - Interfaces and Abstraction - Border Control

---
+++ 05 - C# OOP Advanced/01.Interfaces and Abstraction/05.BorderControl/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05.BorderControl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var entrants = new List<IIdentifiable>();
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "End")
+            {
+                var tokens = inputLine.Split(' ');
+                if (tokens.Length == 2)
+                {
+                    entrants.Add(new Robot(tokens[0], tokens[1]));
+                }
+                else
+                {
+                    entrants.Add(new Citizen(tokens[0], int.Parse(tokens[1]), tokens[2]));
+                }
+            }
+
+            var lastDigits = Console.ReadLine();
+
+            foreach (var entry in entrants.Where(e => e.Id.EndsWith(lastDigits)))
+            {
+                Console.WriteLine(entry.Id);
+            }
+        }
+    }
+}
