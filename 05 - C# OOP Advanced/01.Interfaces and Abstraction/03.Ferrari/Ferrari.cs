1d91453da65b9147b997459741ae8181c496b9ec
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-18 22:37:43 +0300

    C# OOP Advanced - Interfaces and Abstraction - Ferrari

---
+++ 05 - C# OOP Advanced/01.Interfaces and Abstraction/03.Ferrari/Ferrari.cs
@@ -0,0 +1,28 @@
+﻿namespace _03.Ferrari
+{
+    public class Ferrari : ICar
+    {
+        public Ferrari(string driver)
+        {
+            this.Driver = driver;
+        }
+
+        public string Model => "488-Spider";
+        public string Driver { get; }
+
+        public string PushBrakesPedal()
+        {
+            return "Brakes!";
+        }
+
+        public string PushGasPedal()
+        {
+            return "Zadu6avam sA!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Model}/{this.PushBrakesPedal()}/{this.PushGasPedal()}/{this.Driver}";
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/01.Interfaces and Abstraction/03.Ferrari/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03.Ferrari
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string driversName = Console.ReadLine();
+            var ferrari = new Ferrari(driversName);
+            Console.WriteLine(ferrari);
+
+            string ferrariName = typeof(Ferrari).Name;
+            string iCarInterfaceName = typeof(ICar).Name;
+
+            bool isCreated = typeof(ICar).IsInterface;
+            if (!isCreated)
+            {
+                throw new Exception("No interface ICar was created");
+            }
+        }
+    }
+}
