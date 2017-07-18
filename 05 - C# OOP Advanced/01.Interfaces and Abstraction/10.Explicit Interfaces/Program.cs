e85c55658d553ca2347e44a8f433434254faf19a
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-18 22:50:47 +0300

    C# OOP Advanced - Interfaces and Abstraction - Explicit Interface

---
+++ 05 - C# OOP Advanced/01.Interfaces and Abstraction/10.Explicit Interfaces/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _10.Explicit_Interfaces
+{
+    using System;
+    using Entites;
+    using Interfaces;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string inputCitizen;
+            while ((inputCitizen = Console.ReadLine()) != "End")
+            {
+                var tokens = inputCitizen.Split(' ');
+                var citizen = new Citizen(tokens[0], tokens[1], int.Parse(tokens[2]));
+                IResident resident = citizen;
+                IPerson person = citizen;
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
