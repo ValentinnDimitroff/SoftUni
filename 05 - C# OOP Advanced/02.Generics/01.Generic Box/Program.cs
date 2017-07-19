76ec477b59e3e7085017345a73f7f40f7430b235
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-19 11:22:33 +0300

    C# OOP Advanced - Generics - Generic Box of String

---
+++ 05 - C# OOP Advanced/02.Generics/01.Generic Box/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.Generic_Box
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numberOfStrings = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                var currentString = Console.ReadLine();
+                Console.WriteLine(new Box<string>(currentString));
+            }
+        }
+    }
+}
