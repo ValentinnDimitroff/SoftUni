6f8099d1ee86a650b6a06a54a87de9229bec3545
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-21 00:23:15 +0300

    C# OOP Advanced - Iterators and Comparators - ListyIterator

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,53 @@
+﻿namespace _01.ListyIterator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ListyIterator<T>
+    {
+        private readonly List<T> list;
+        private int currentIndex;
+
+        public ListyIterator() : this(Enumerable.Empty<T>())
+        {
+        }
+
+        public ListyIterator(IEnumerable<T> collection)
+        {
+            this.list = new List<T>(collection);
+            this.currentIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this.currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.currentIndex < this.list.Count - 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.list.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.list[this.currentIndex]);
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/01.ListyIterator/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.ListyIterator
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputCommand = Console.ReadLine();
+            var listyIterator = new ListyIterator<string>(inputCommand.Split(' ').Skip(1));
+
+            while ((inputCommand = Console.ReadLine()).ToLower() != "end")
+            {
+                var tokens = inputCommand.Split(' ');
+
+                switch (tokens[0])
+                {
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            listyIterator.Print();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
