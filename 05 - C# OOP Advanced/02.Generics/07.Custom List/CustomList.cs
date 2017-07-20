3592f797a103ca26901adfa70bcd9d6c42d1aa83
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-20 08:07:23 +0300

    C# OOP Advanced - Generics - Custom List

---
+++ 05 - C# OOP Advanced/02.Generics/07.Custom List/CustomList.cs
@@ -0,0 +1,54 @@
+﻿namespace _07.Custom_List
+{
+    using System;
+    using System.Collections;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class CustomList<T> : ICustomList<T>, IEnumerable<T>
+        where T : IComparable
+    {
+        private List<T> list;
+
+        public CustomList()
+        {
+            this.list = new List<T>();
+        }
+        
+        public void Add(T element) => this.list.Add(element);
+
+        public T Remove(int index)
+        {
+            var temp = this.list[index];
+            this.list.RemoveAt(index);
+            return temp;
+        }
+
+        public bool Contains(T element) => this.list.Contains(element);
+
+        public void Swap(int index1, int index2)
+        {
+            var temp = this.list[index1];
+            this.list[index1] = this.list[index2];
+            this.list[index2] = temp;
+        }
+
+        public int CountGreaterThan(T element) => this.list.Count(x => x.CompareTo(element) > 0);
+
+        public T Max() => this.list.Max();
+        
+        public T Min() => this.list.Min();
+
+        public IEnumerator<T> GetEnumerator() => this.list.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public T this[int index]
+        {
+            get { return this.list[index]; }
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/02.Generics/07.Custom List/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _07.Custom_List
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var myCustomList = new CustomList<string>();
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "END")
+            {
+                var args = inputLine.Split(' ');
+
+                switch (args[0])
+                {
+                    case "Add":
+                        myCustomList.Add(args[1]);
+                        break;
+                    case "Remove":
+                        myCustomList.Remove(int.Parse(args[1]));
+                        break;
+                    case "Contains":
+                        Console.WriteLine(myCustomList.Contains(args[1]));
+                        break;
+                    case "Swap":
+                        myCustomList.Swap(int.Parse(args[1]), int.Parse(args[2]));
+                        break;
+                    case "Greater":
+                        int count = myCustomList.CountGreaterThan(args[1]);
+                        Console.WriteLine(count);
+                        break;
+                    case "Max":
+                        Console.WriteLine(myCustomList.Max());
+                        break;
+                    case "Min":
+                        Console.WriteLine(myCustomList.Min());
+                        break;
+                    case "Print":
+                        foreach (var element in myCustomList)
+                        {
+                            Console.WriteLine(element);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
