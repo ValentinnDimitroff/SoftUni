e46f874ec86c3a282d7a86a9a1f2b307d1fbb486
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-20 08:05:34 +0300

    C# OOP Advanced - Generics - Swap Method String

---
+++ 05 - C# OOP Advanced/02.Generics/03.Generic Swap Method Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _03.Generic_Swap_Method_Strings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var boxes = new List<Box<string>>();
+            var numberOfStrings = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                var currentString = Console.ReadLine();
+                boxes.Add(new Box<string>(currentString));
+            }
+
+            var indexes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            SwapElements(boxes, indexes[0], indexes[1]);
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+        public static void SwapElements<T> (List<T> list, int index1, int index2)
+        {
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+    }
+}
