7ccbae8272a1913f8a1b3c6067e752b31f7acb0c
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-20 08:06:33 +0300

    C# OOP Advanced - Generics - Generic Count Method Strings

---
+++ 05 - C# OOP Advanced/02.Generics/05.Generic Count Method Strings/Box.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.Generic_Count_Method_Strings
+{
+    using System;
+    public class Box<T> : IComparable<Box<T>> where T : IComparable
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType().FullName}: {this.Value}";
+        }
+
+        public int CompareTo(Box<T> otherBox)
+        {
+            return this.Value.CompareTo(otherBox.Value);
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/02.Generics/05.Generic Count Method Strings/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _05.Generic_Count_Method_Strings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var boxes = new List<Box<string>>();
+            var numberOfStrings = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                var currentString = Console.ReadLine();
+                boxes.Add(new Box<string>(currentString));
+            }
+
+            var element = new Box<string>(Console.ReadLine());
+            var result = GetGreaterElementsCount(boxes, element);
+            Console.WriteLine(result);
+        }
+
+        public static int GetGreaterElementsCount<T>(List<T> list, T element)
+            where T : IComparable<T>
+        {
+            return list.Count(x => x.CompareTo(element) > 0);
+        }
+
+        //public static int GetGreaterElementsCount<T> (List<Box<T>> list, Box<T> element)
+        //    where T : IComparable
+        //{
+        //    return list.Count(x => x.Value.CompareTo(element.Value) > 0);
+        //}
+    }
+}
