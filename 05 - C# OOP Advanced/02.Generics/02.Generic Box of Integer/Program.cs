923e5531385a1177dcdbbdcb41d8e9c5d3d6a023
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-20 08:05:02 +0300

    C# OOP Advanced - Generics - Generic Box Of Integer

---
+++ 05 - C# OOP Advanced/02.Generics/02.Generic Box of Integer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.Generic_Box_of_Integer
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numberOfStrings = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                var currentString = int.Parse(Console.ReadLine());
+                Console.WriteLine(new Box<int>(currentString));
+            }
+        }
+    }
+}
