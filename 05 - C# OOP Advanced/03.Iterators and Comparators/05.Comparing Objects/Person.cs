333711d894a5951193a6f73b07fabc4e0240c53a
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-24 22:08:01 +0300

    C# OOP Advanced - Iterators and Comparators - Comparing Objects

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/05.Comparing Objects/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.Comparing_Objects
+{
+    using System;
+
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town) 
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Town { get; private set; }
+
+        public int CompareTo(Person person)
+        {
+            int result;
+            if ((result = this.Name.CompareTo(person.Name)) != 0)
+            {
+                return result;
+            }
+
+            if ((result = this.Age.CompareTo(person.Age)) != 0)
+            {
+                return result;
+            }
+
+            return this.Town.CompareTo(person.Town);
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/05.Comparing Objects/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.Comparing_Objects
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            IList<Person> people = new List<Person>();
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "END")
+            {
+                var tokens = inputLine.Split(' ');
+                people.Add(new Person(tokens[0], int.Parse(tokens[1]), tokens[2]));
+            }
+
+            var personIndex = int.Parse(Console.ReadLine());
+            var equalPeople = people.Count(p => p.CompareTo(people[personIndex - 1]) == 0);
+
+            if (equalPeople > 1)
+                Console.WriteLine($"{equalPeople} {people.Count - equalPeople} {people.Count }");
+            else
+                Console.WriteLine("No matches"); 
+        }
+    }
+}
