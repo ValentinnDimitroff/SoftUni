b5059a157c922f2f96b3d585b53bbc851d119a25
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-24 22:07:33 +0300

    C# OOP Advanced - Iterators and Comparators - Strategy Pattern

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/06.Strategy Pattern/Comparators/PersonNameComparer.cs
@@ -0,0 +1,20 @@
+﻿namespace _06.Strategy_Pattern.Comparators
+{
+    using System.Collections.Generic;
+
+    public class PersonNameComparer : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            int result = x.Name.Length.CompareTo(y.Name.Length);
+            if (result == 0)
+            {
+                char xLetter = char.ToLower(x.Name[0]);
+                char yLetter = char.ToLower(y.Name[0]);
+                result = xLetter.CompareTo(yLetter);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/06.Strategy Pattern/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _06.Strategy_Pattern
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Comparators;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            SortedSet<Person> peopleOrderedByName = new SortedSet<Person>(new PersonNameComparer());
+            SortedSet<Person> peopleOrderedByAge = new SortedSet<Person>(new PersonAgeComparator());
+
+            var peopleCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < peopleCount; i++)
+            {
+                var personInfo = Console.ReadLine()
+                    .Split(' ');
+                Person person  = new Person(personInfo[0], int.Parse(personInfo[1]));
+
+                peopleOrderedByName.Add(person);
+                peopleOrderedByAge.Add(person);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, peopleOrderedByName));
+            Console.WriteLine(string.Join(Environment.NewLine, peopleOrderedByAge));
+        }
+    }
+}
