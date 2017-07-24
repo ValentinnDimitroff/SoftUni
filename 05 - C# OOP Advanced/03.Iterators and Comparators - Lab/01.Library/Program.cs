0e542491fcce367bb587183307e95b88b5e1cbc6
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-24 22:09:54 +0300

    C# OOP Advanced - Iterators and Comparators - Library

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators - Lab/01.Library/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.Library
+{
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookThree = new Book("The Documents in the Case", 2002);
+            Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+            IList<Book> books = new List<Book>();
+            books.Add(bookOne);
+            books.Add(bookTwo);
+            books.Add(bookThree);
+        }
+    }
+}
