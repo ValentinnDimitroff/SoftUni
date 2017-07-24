b7794dcd9999a4fe0bea5e2e401ef063ab7f4a73
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-24 22:06:26 +0300

    C# OOP Advanced - Iterators and Comparators - Froggy

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/04.Froggy/Lake.cs
@@ -0,0 +1,51 @@
+﻿namespace _04.Froggy
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Lake : IEnumerable<int>
+    {
+        private IList<int> listOfStones;
+
+        public Lake(IEnumerable<int> collection)
+        {
+            this.listOfStones = new List<int>(collection);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            //for (int i = 1; i <= this.listOfStones.Count; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        yield return this.listOfStones[i - 1];
+            //    }
+            //}
+
+            //for (int i = this.listOfStones.Count; i >= 1; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        yield return this.listOfStones[i - 1];
+            //    }
+            //}
+
+            for (int i = 0; i < this.listOfStones.Count; i += 2)
+            {
+                yield return this.listOfStones[i];
+            }
+
+            int lastEvenPosition = this.listOfStones.Count % 2 == 0 
+                ? this.listOfStones.Count - 1 
+                : this.listOfStones.Count - 2;
+
+            for (int i = lastEvenPosition; i > 0; i -= 2)
+            {
+                yield return this.listOfStones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/04.Froggy/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.Froggy
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var stones = Console.ReadLine()
+                .Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var lake = new Lake(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
