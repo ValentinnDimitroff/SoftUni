e1f3272d1f9d3818a53cc8bd897227a038f37c0e
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-24 22:08:39 +0300

    C# OOP Advanced - Iterators and Comparators - Equalty Logic

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/07.Equalty Logic/Person.cs
@@ -0,0 +1,53 @@
+﻿namespace _07.Equalty_Logic
+{
+    using System;
+
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            Person other = obj as Person;
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (this.CompareTo(other) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() ^ this.Age ^ 231;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Age}";
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/07.Equalty Logic/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _07.Equalty_Logic
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            HashSet<Person> peopleHashed = new HashSet<Person>();
+            SortedSet<Person> peopleSorted = new SortedSet<Person>();
+
+            var peopleCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < peopleCount; i++)
+            {
+                var personInfo = Console.ReadLine()
+                    .Split(' ');
+                Person person = new Person(personInfo[0], int.Parse(personInfo[1]));
+
+                peopleHashed.Add(person);
+                peopleSorted.Add(person);
+            }
+
+            Console.WriteLine(peopleHashed.Count);
+            Console.WriteLine(peopleSorted.Count);
+        }
+    }
+}
