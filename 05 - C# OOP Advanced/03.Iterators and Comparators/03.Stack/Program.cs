88ccc117c5683eff8fdeb5b062a4cb570692ce8e
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-24 22:09:04 +0300

    C# OOP Advanced - Iterators and Comparators - Stack

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/03.Stack/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _03.Stack
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var myStack = new Stack<int>();
+
+            string inputLine;
+            while ((inputLine = Console.ReadLine()) != "END")
+            {
+                var tokens = inputLine
+                    .Split(new []{ ' ' , ','}, StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+
+                var command = tokens[0];
+                tokens.Remove(command);
+
+                switch (command)
+                {
+                    case "Push":
+                        foreach (var token in tokens)
+                        {
+                            myStack.Push(int.Parse(token));
+                        }
+                        break;
+                    case "Pop":
+                        try
+                        {
+                            myStack.Pop();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var element in myStack)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/03.Stack/Stack.cs
@@ -0,0 +1,63 @@
+﻿namespace _03.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Stack<T> : IEnumerable<T>
+    {
+        private const int InitialCapacity = 16;
+        private T[] collection;
+
+        public Stack(int capacity = InitialCapacity)
+        {
+            this.collection = new T[capacity];
+        }
+
+        public int Capacity { get => this.collection.Length; }
+
+        public int Count { get; set; }
+
+        public void Push(T element)
+        {
+            if (this.Count == this.Capacity)
+            {
+                this.Resize();
+            }
+            this.collection[this.Count] = element;
+            this.Count++;
+        }
+
+        private void Resize()
+        {
+            var newCollection = new T[2 * this.collection.Length];
+            Array.Copy(this.collection, newCollection, this.Count);
+            this.collection = newCollection;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new ArgumentException("No elements");
+            }
+
+            var temp = this.collection[this.Count];
+            this.Count--;
+            return temp;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                yield return this.collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
