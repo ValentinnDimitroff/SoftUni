28b3f0e05fdb93b8be349abf0fcb726286a2b81b
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-31 00:25:40 +0300

    C# OOP Advanced  - Iterators and Comparators - Pet Clinic

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/08.Pet Clinic/Entites/Clinic.cs
@@ -0,0 +1,96 @@
+﻿namespace _08.Pet_Clinic.Entites
+{
+    using System;
+    using System.Text;
+    using System.Threading;
+
+    public class Clinic
+    {
+        private int roomsNumber;
+        private int occupiedRooms;
+        private RoomsRegister roomsRegister;
+
+        public Clinic(string name, int roomsNumber)
+        {
+            this.Name = name;
+            this.RoomsNumber = roomsNumber;
+            this.roomsRegister = new RoomsRegister(roomsNumber);
+            this.occupiedRooms = 0;
+        }
+        
+        public string Name { get; set; }
+
+        public int RoomsNumber
+        {
+            get => this.roomsNumber;
+            set
+            {
+                if (value % 2 == 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                this.roomsNumber = value;
+            }
+        }
+
+        public bool TryAddPet(Pet pet)
+        {
+            foreach (var roomIndex in this.roomsRegister)
+            {
+                if (this.roomsRegister[roomIndex] == null)
+                {
+                    this.roomsRegister[roomIndex] = pet;
+                    this.occupiedRooms++;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool TryReleasePet()
+        {
+            int centerRoom = this.RoomsNumber / 2;
+            for (int i = 0; i < this.RoomsNumber; i++)
+            {
+                if (this.roomsRegister[(centerRoom + i) % this.RoomsNumber] != null)
+                {
+                    this.roomsRegister[(centerRoom + i) % this.RoomsNumber] = null;
+                    this.occupiedRooms--;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasEmptyRooms()
+        {
+            return this.occupiedRooms < this.RoomsNumber;
+        }
+
+        public string Print()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < this.roomsNumber; i++)
+            {
+                if (this.roomsRegister[i] != null)
+                {
+                    sb.AppendLine(this.Print(i));
+                }
+                else
+                {
+                    sb.AppendLine("Room empty");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public string Print(int roomIndex)
+        {
+            return this.roomsRegister[roomIndex]?.ToString() ?? "Room empty";
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/08.Pet Clinic/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace _08.Pet_Clinic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using Entites;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var allClinics = new Dictionary<string, Clinic>();
+            var allPets = new Dictionary<string, Pet>();
+
+            var commandsCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < commandsCount; i++)
+            {
+                var commandTokens = Console.ReadLine().Split();
+
+                switch (commandTokens[0])
+                {
+                    case "Create":
+                        if (commandTokens[1] == "Clinic")
+                        {
+                            try
+                            {
+                                allClinics.Add(commandTokens[2],
+                                    new Clinic(commandTokens[2], int.Parse(commandTokens[3])));
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Invalid Operation!");
+                            }
+                        }
+                        else if (commandTokens[1] == "Pet")
+                        {
+                            allPets.Add(commandTokens[2],
+                                new Pet(commandTokens[2], int.Parse(commandTokens[3]), commandTokens[4]));
+                        }
+                        break;
+                    case "Add":
+                        var pet = allPets[commandTokens[1]];
+                        var apointedClinic = allClinics[commandTokens[2]];
+                        if (apointedClinic.TryAddPet(pet))
+                        {
+                            Console.WriteLine(true);
+                            allPets.Remove(pet.Name);
+                        }
+                        else
+                        {
+                            Console.WriteLine(false);
+                        }
+                        break;
+                    case "Release":
+                        Console.WriteLine(allClinics[commandTokens[1]].TryReleasePet());
+                        break;
+                    case "HasEmptyRooms":
+                        Console.WriteLine(allClinics[commandTokens[1]].HasEmptyRooms());
+                        break;
+                    case "Print":
+                        if (commandTokens.Length == 2)
+                        {
+                            Console.WriteLine(allClinics[commandTokens[1]].Print());
+                        }
+                        else
+                        {
+                            Console.WriteLine(allClinics[commandTokens[1]].Print(int.Parse(commandTokens[2]) - 1));
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
