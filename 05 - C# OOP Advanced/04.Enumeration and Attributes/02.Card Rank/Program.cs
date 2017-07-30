6291fe59d640f55a19896edb346a998998ec4c20
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-31 00:39:57 +0300

    C# OOP Advanced  - Enums and Attributes - Card Rank

---
+++ 05 - C# OOP Advanced/04.Enumeration and Attributes/02.Card Rank/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02.Card_Rank
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Card Ranks:");
+            foreach (var cardValue in Enum.GetValues(typeof(CardRank)))
+            {
+                Console.WriteLine($"Ordinal value: {(int)cardValue}; Name value: {cardValue}");
+            }
+        }
+    }
+}
