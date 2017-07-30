ca2d0dea88b75eb9fdd9250d3b8f8dbb5d7d7c8a
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-31 00:50:10 +0300

    C# OOP Advanced  - Enums and Attributes - Deck Of Cards

---
+++ 05 - C# OOP Advanced/04.Enumeration and Attributes/07.Deck Of Cards/Enums/CardSuit.cs
@@ -0,0 +1,10 @@
+﻿namespace _07.Deck_Of_Cards.Enums
+{
+    public enum CardSuit
+    {
+        Clubs = 0,
+        Diamonds = 13,
+        Hearts = 26,
+        Spades = 39
+    }
+}
---
+++ 05 - C# OOP Advanced/04.Enumeration and Attributes/07.Deck Of Cards/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _07.Deck_Of_Cards
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Enums;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            IEnumerable<CardSuit> allSuits = Enum.GetValues(typeof(CardSuit)).Cast<CardSuit>();
+            IEnumerable<CardRank> allRanks = Enum.GetValues(typeof(CardRank)).Cast<CardRank>();
+
+            
+        }
+    }
+}
