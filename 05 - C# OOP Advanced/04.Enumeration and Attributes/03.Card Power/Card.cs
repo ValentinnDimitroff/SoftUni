bdfc07bd52bd9ce9ad626d09b2f0a42d8bd5942e
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-31 00:40:44 +0300

    C# OOP Advanced  - Enums and Attributes - Card Power

---
+++ 05 - C# OOP Advanced/04.Enumeration and Attributes/03.Card Power/Card.cs
@@ -0,0 +1,25 @@
+﻿namespace _03.Card_Power
+{
+    using System;
+
+    public class Card 
+    {
+        public Card(CardRank rank, CardSuit suit)
+        {
+            this.Suit = suit;
+            this.Rank = rank;
+            this.Power = (int)suit + (int)rank;
+        }
+
+        public CardSuit Suit { get; set; }
+
+        public CardRank Rank { get; set; }
+
+        public int Power { get; set; }
+
+        public override string ToString()
+        {
+            return $"Card name: {this.Rank} of {this.Suit}; Card power: {this.Power}";
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/04.Enumeration and Attributes/03.Card Power/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03.Card_Power
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CardRank cardsRank = (CardRank)Enum.Parse(typeof(CardRank), Console.ReadLine());
+            CardSuit cardsSuit = (CardSuit)Enum.Parse(typeof(CardSuit), Console.ReadLine());
+
+            Card myCard = new Card(cardsRank, cardsSuit);
+            Console.WriteLine(myCard);
+        }
+    }
+}
