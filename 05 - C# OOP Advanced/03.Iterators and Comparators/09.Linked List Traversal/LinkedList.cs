172c9a8078b642f8097f309c5b7fca0c1647def5
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-31 00:30:06 +0300

    C# OOP Advanced  - Iterators and Comparators - Linked List

---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/09.Linked List Traversal/LinkedList.cs
@@ -0,0 +1,104 @@
+﻿namespace _09.Linked_List_Traversal
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class LinkedList<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; set; }
+
+        public void Add(T element)
+        {
+            Node newNode = new Node(element);
+            if (this.IsEmpty())
+            {
+                this.Head = newNode;
+                this.Count++;
+                return;
+            }
+
+            if (this.Tail == null)
+            {
+                this.Tail = newNode;
+                this.Head.Next = this.Tail;
+                this.Count++;
+                return;
+            }
+
+            this.Tail.Next = newNode;
+            this.Tail = newNode;
+            this.Count++;
+        }
+
+        public void Remove(T element)
+        {
+            if (this.IsEmpty())
+            {
+                return;
+            }
+
+            if (this.Head.Value.CompareTo(element) == 0)
+            {
+                this.Head = this.Head.Next;
+                this.Count--;
+                return;
+            }
+
+            Node currentNode = this.Head;
+            while (currentNode.Next != null)
+            {
+                if (currentNode.Next.Value.CompareTo(element) == 0)
+                {
+                    if (this.Tail == currentNode.Next)
+                    {
+                        this.Tail = currentNode;
+                        this.Tail.Next = null;
+                    }
+                    else
+                    {
+                        currentNode.Next = currentNode.Next.Next;
+                    }
+                    this.Count--;
+                    break;
+                }
+                currentNode = currentNode.Next;
+            }
+
+        }
+
+        private bool IsEmpty()
+        {
+            return this.Head == null;
+        }
+
+        public class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+            public T Value { get; set; }
+            public Node Next { get; set; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node currentNode = this.Head;
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/03.Iterators and Comparators/09.Linked List Traversal/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _09.Linked_List_Traversal
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            LinkedList<int> myLinkedList = new LinkedList<int>();
+
+            int inputCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputCount; i++)
+            {
+                string[] commandTokens = Console.ReadLine().Split(' ');
+                switch (commandTokens[0])
+                {
+                    case "Add":
+                        myLinkedList.Add(int.Parse(commandTokens[1]));
+                        break;
+                    case "Remove":
+                        myLinkedList.Remove(int.Parse(commandTokens[1]));
+                        break;
+                }
+            }
+
+            Console.WriteLine(myLinkedList.Count);
+            Console.WriteLine(string.Join(" ", myLinkedList));
+        }
+    }
+}
