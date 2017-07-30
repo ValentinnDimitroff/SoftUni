4119cb431518f2a71754ec6ef05c87779534ea40
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-31 00:39:30 +0300

    C# OOP Advanced  - Enums and Attributes - Traffic Lights

---
+++ 05 - C# OOP Advanced/04.Enumeration and Attributes/09.Traffic Lights/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _09.Traffic_Lights
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<TrafficLight> trafficLights = new List<TrafficLight>();
+            string[] inputSignals = Console.ReadLine().Split();
+            int stateChangeCount = int.Parse(Console.ReadLine());
+
+            foreach (string inputSignal in inputSignals)
+            {
+                Light currentLight = (Light)Enum.Parse(typeof(Light), inputSignal);
+                trafficLights.Add(new TrafficLight(currentLight));
+            }
+
+            for (int i = 0; i < stateChangeCount; i++)
+            {
+                foreach (TrafficLight trafficLight in trafficLights)
+                {
+                    trafficLight.SwitchState();
+                }
+                Console.WriteLine(string.Join(" ", trafficLights));
+            }
+        }
+    }
+}
---
+++ 05 - C# OOP Advanced/04.Enumeration and Attributes/09.Traffic Lights/TrafficLight.cs
@@ -0,0 +1,24 @@
+﻿namespace _09.Traffic_Lights
+{
+    using System;
+
+    public class TrafficLight
+    {
+        public TrafficLight(Light lightState)
+        {
+            this.LightState = lightState;
+        }
+
+        public Light LightState { get; set; }
+
+        public void SwitchState()
+        {
+            this.LightState = (Light)((int)++this.LightState % 3);
+        }
+
+        public override string ToString()
+        {
+            return this.LightState.ToString();
+        }
+    }
+}
