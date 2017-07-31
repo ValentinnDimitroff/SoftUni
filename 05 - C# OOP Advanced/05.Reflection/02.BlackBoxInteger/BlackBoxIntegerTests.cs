4014e3436327dad8c7c17ff4797ef6045f77aab0
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-07-31 14:19:52 +0300

    C# OOP Advanced - Reflection - Black Box Integer

---
+++ 05 - C# OOP Advanced/05.Reflection/02.BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,44 @@
+﻿namespace _02BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main()
+        {
+            Type blackBoxIntType = typeof(BlackBoxInt);
+            ConstructorInfo blackBoxCtor = blackBoxIntType
+                .GetConstructor(
+                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
+                    null,
+                    Type.EmptyTypes,
+                    null);
+            BlackBoxInt myBlackBox = (BlackBoxInt)blackBoxCtor.Invoke(new object[] {});
+
+            FieldInfo[] fields = blackBoxIntType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            string fieldName = fields.FirstOrDefault().Name;
+
+            string inputRequest;
+            while ((inputRequest = Console.ReadLine()) != "END")
+            {
+                string[] tokens = inputRequest.Split('_');
+                string methodName = tokens[0];
+                int value = int.Parse(tokens[1]);
+
+                blackBoxIntType
+                    .GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Invoke(myBlackBox, new object[] {value});
+
+                string innerState = blackBoxIntType
+                    .GetField(
+                        fieldName,
+                        BindingFlags.Instance | BindingFlags.NonPublic)
+                    .GetValue(myBlackBox).ToString();
+
+                Console.WriteLine(innerState);
+            }
+        }
+    }
+}
