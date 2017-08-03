467eefbb5791a60f793f3118190fa9262d4aa061
ValentinDimitrov <v.dimitroff@yahoo.com>
2017-08-03 17:46:33 +0300

    C# OOP Advanced - Reflection Lab - Private Spy

---
+++ 05 - C# OOP Advanced/05.Reflection - Lab/01. Private Spy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        Spy spy = new Spy();
+        string result = spy.CollectGettersAndSetters("Hacker");
+        Console.WriteLine(result);
+    }
+}
